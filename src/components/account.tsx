e62d7d8df1327e0a31f536cdf0a30866bb885b14
win11905 <donothavemoney@gmail.com>
2019-07-22 23:05:46 +0700

    split Login Session to new component (account.tsx)

---
+++ src/components/account.tsx
@@ -0,0 +1,112 @@
+/* React */
+import React from "react"
+
+/* Data model */
+import firebase from "firebase/app"
+import "firebase/auth"
+
+interface accountState {
+  toggleDetail: Boolean,
+  sign: Boolean,
+  displayName: String,
+  avatar: String
+}
+
+class AccountControl extends React.Component<{}, accountState> {
+  constructor(props:any){
+    super(props);
+    this.state = {
+      toggleDetail: false,
+      sign: false,
+      displayName: "",
+      avatar: ""
+    }
+  }
+
+  componentWillMount(){
+    firebase.auth().onAuthStateChanged(account => {
+      if(account){
+        // Signed in.
+        this.setState({
+          sign:true,
+          displayName: account.displayName ? account.displayName : "",
+          avatar: account.photoURL ? account.photoURL : ""
+        })
+      } else {
+      }
+    })
+  }
+
+  toggleDetail(){
+    this.setState({
+      toggleDetail: !this.state.toggleDetail
+    })
+  }
+
+  loginWithGmail(){
+    let provider = new firebase.auth.GoogleAuthProvider();
+    this.toggleDetail();
+    firebase.auth().signInWithPopup(provider).then(function(result:any) {
+        // This gives you a Google Access Token. You can use it to access the Google API.
+        var token = result.credential.accessToken;
+        // The signed-in user info.
+        var user = result.user;
+      }).catch(function(error:any) {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        console.log(`${errorCode}: ${errorMessage}`);
+        // The email of the user's account used.
+        var email = error.email;
+        // The firebase.auth.AuthCredential type that was used.
+        var credential = error.credential;
+        // ...
+    });
+  }
+
+  firebaseLogout(){
+    firebase.auth().signOut().then(() => {
+      window.location.reload();
+    })
+  }
+
+  
+  render(){
+    return(
+      <>
+        <button id="user-panel" onClick={() => this.toggleDetail()}>
+          {this.state.sign ?
+            <>
+              <p>{this.state.displayName}</p>
+              <img src={`${this.state.avatar}`} />
+            </>
+          :
+            <>
+              <p>Guest</p>
+              <img src="/assets/img/default-user.png" />
+            </>
+          }
+        </button>
+        {this.state.toggleDetail ?
+          <div id="user-control">
+            {this.state.sign ?
+              <button onClick={() => this.firebaseLogout()}>
+                <i className="material-icons">vpn_key</i>
+                Logout
+              </button>
+            :
+              <button onClick={() => this.loginWithGmail()}>
+                <i className="material-icons">email</i>
+                Login with Gmail
+              </button>
+            }
+          </div>
+        : null}
+      </>
+    )
+  }
+}
+
+export default function AccountRender() {
+    return(<AccountControl/>);
+}
--- src/components/nav.tsx
+++ src/components/nav.tsx
@@ -3,8 +3,9 @@ import React, {Component, useState} from "react"
 
 /* React Component */
 import { Link } from "react-router-dom"
+import LeftDrawer from "./left_drawer"
+import AccountRender from "./account"
 import "../assets/css/nav.css"
-import SwipeableTemporaryDrawer from "./left_drawer"
 
 /* Data model */
 import firebase from "firebase/app"
@@ -26,129 +27,12 @@ const LinkIcon = (props:NavProps) => {
     )
 }
 
-interface ActionProps {
-  icon: String,
-  onClick?: any,
-  id?: String,
-}
-
-const ActionIcon = (props:ActionProps) => {
-  return(
-    <button id={'props.id'} className="action-icon" onClick={() => props.onClick()}>
-      <a className="nav-icon action-icon-responsive">
-        <i className="material-icons">{props.icon}</i>
-      </a>
-    </button>
-  )
-}
-
-interface accountState {
-  toggleDetail: Boolean,
-  sign: Boolean,
-  displayName: String,
-  avatar: String
-}
-
-class AccountControl extends React.Component<{}, accountState> {
-  constructor(props:any){
-    super(props);
-    this.state = {
-      toggleDetail: false,
-      sign: false,
-      displayName: "",
-      avatar: ""
-    }
-  }
-
-  componentWillMount(){
-    firebase.auth().onAuthStateChanged(account => {
-      if(account){
-        // Signed in.
-        this.setState({
-          sign:true,
-          displayName: account.displayName ? account.displayName : "",
-          avatar: account.photoURL ? account.photoURL : ""
-        })
-      } else {
-      }
-    })
-  }
-
-  toggleDetail(){
-    this.setState({
-      toggleDetail: !this.state.toggleDetail
-    })
-  }
-
-  loginWithGmail(){
-    let provider = new firebase.auth.GoogleAuthProvider();
-    this.toggleDetail();
-    firebase.auth().signInWithPopup(provider).then(function(result:any) {
-        // This gives you a Google Access Token. You can use it to access the Google API.
-        var token = result.credential.accessToken;
-        // The signed-in user info.
-        var user = result.user;
-      }).catch(function(error:any) {
-        // Handle Errors here.
-        var errorCode = error.code;
-        var errorMessage = error.message;
-        console.log(`${errorCode}: ${errorMessage}`);
-        // The email of the user's account used.
-        var email = error.email;
-        // The firebase.auth.AuthCredential type that was used.
-        var credential = error.credential;
-        // ...
-    });
-  }
-
-  firebaseLogout(){
-    firebase.auth().signOut().then(() => {
-      window.location.reload();
-    })
-  }
-
-  render(){
-    return(
-      <>
-        <button id="user-panel" onClick={() => this.toggleDetail()}>
-          {this.state.sign ?
-            <>
-              <p>{this.state.displayName}</p>
-              <img src={`${this.state.avatar}`} />
-            </>
-          :
-            <>
-              <p>Guest</p>
-              <img src="/assets/img/default-user.png" />
-            </>
-          }
-        </button>
-        {this.state.toggleDetail ?
-          <div id="user-control">
-            {this.state.sign ?
-              <button onClick={() => this.firebaseLogout()}>
-                <i className="material-icons">vpn_key</i>
-                Logout
-              </button>
-            :
-              <button onClick={() => this.loginWithGmail()}>
-                <i className="material-icons">email</i>
-                Login with Gmail
-              </button>
-            }
-          </div>
-        : null}
-      </>
-    )
-  }
-}
-
 export default () => {
     return(
       <>
         <nav id="main-nav">
           <div>
-            <SwipeableTemporaryDrawer />            
+            <LeftDrawer />            
             <LinkIcon icon="home" to="/" />
             <LinkIcon icon="functions" to="/tasks" />
             <LinkIcon icon="school" to="/learn" />
@@ -156,7 +40,7 @@ export default () => {
             <LinkIcon icon="featured_play_list" to="/exam" />
           </div>
           <div style={{display:"inline-flex",flexDirection:"row-reverse"}}>
-            <AccountControl />
+            <AccountRender />
           </div>
         </nav>
       </>
