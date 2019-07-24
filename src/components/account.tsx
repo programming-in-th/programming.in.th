afa4d23256bbc4043c341c0a88db86115f187347
PeppaPigHS <ppeppapig.stuff@gmail.com>
2019-07-24 14:12:30 +0700

    Creating login with email interface

--- src/components/account.tsx
+++ src/components/account.tsx
@@ -1,13 +1,18 @@
 /* React */
-import React from "react"
+import React, { useState } from "react"
 
 /* Data model */
 import firebase from "firebase/app"
 import "firebase/auth"
 import StyledFirebaseAuth from "react-firebaseui/StyledFirebaseAuth"
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+import Fade from '@material-ui/core/Fade';
+
 
 interface accountState {
   toggleDetail: Boolean,
+  toggleSignUp: Boolean,
   sign: Boolean,
   displayName: String,
   avatar: String
@@ -17,7 +22,8 @@ class Account extends React.Component<{}, accountState> {
   constructor(props:any){
     super(props);
     this.state = {
-      toggleDetail: false,
+      toggleDetail: false, 
+      toggleSignUp: false,
       sign: false,
       displayName: "",
       avatar: ""
@@ -31,7 +37,6 @@ class Account extends React.Component<{}, accountState> {
     signInOptions: [
       firebase.auth.GoogleAuthProvider.PROVIDER_ID,
       firebase.auth.FacebookAuthProvider.PROVIDER_ID,
-      firebase.auth.EmailAuthProvider.PROVIDER_ID,
     ],
     callbacks: {
       // Avoid redirects after sign-in.
@@ -63,7 +68,13 @@ class Account extends React.Component<{}, accountState> {
       window.location.reload();
     })
   }
-  
+
+  toggleSignUp() {
+    this.setState({
+      toggleSignUp: !this.state.toggleSignUp
+    })
+  }
+
   render(){
     return(
       <>
@@ -73,13 +84,45 @@ class Account extends React.Component<{}, accountState> {
         </button>
         {this.state.toggleDetail ?
           <div id="user-control">
-            {this.state.sign ?
-              <button onClick={() => this.firebaseLogout()}>
-                <i className="material-icons">vpn_key</i>
-                Logout
-              </button>
-            :
-              <StyledFirebaseAuth uiConfig={this.uiConfig} firebaseAuth={firebase.auth()} /> 
+            {this.state.toggleSignUp ?
+              <Fade in={this.state.toggleSignUp ? true : false}>
+                <div id="account-container">
+                  <TextField id="user-textfield" label="Username" margin="normal" variant="outlined" /> 
+                  <TextField id="email-textfield" label="Email" margin="normal" variant="outlined" /> 
+                  <TextField id="pass-textfield" label="Password" margin="normal" type="password" variant="outlined" /> 
+                  <Button variant="contained" color="primary">
+                    <i className="material-icons">person_add</i>
+                    Sign Up
+                  </Button>
+                  <Button variant="contained" color="secondary" onClick={() => this.toggleSignUp()}>
+                    <i className="material-icons">keyboard_arrow_left</i>
+                    Back to Login
+                  </Button>
+                </div>
+              </Fade>
+              :
+              <>
+              { this.state.sign ?
+                <button onClick={() => this.firebaseLogout()}>
+                  <i className="material-icons">vpn_key</i>
+                  Logout
+                </button>
+              :
+                <div id="account-container">
+                  <TextField id="email-textfield" label="Email" margin="normal" variant="outlined" /> 
+                  <TextField id="pass-textfield" label="Password" margin="normal" type="password" variant="outlined" /> 
+                  <Button variant="contained" color="primary">
+                    <i className="material-icons">account_circle</i>
+                    Log In
+                  </Button>
+                  <Button variant="contained" color="secondary" onClick={() => this.toggleSignUp()}>
+                    <i className="material-icons">person_add</i>
+                    Sign Up
+                  </Button>
+                  <StyledFirebaseAuth uiConfig={this.uiConfig} firebaseAuth={firebase.auth()} /> 
+                </div>
+              }
+              </>
             }
           </div>
         : null}
