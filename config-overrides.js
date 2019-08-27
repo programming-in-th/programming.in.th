c627aad6ba5f43852d5aa0dbb4e086b1cfeed925
iammarkps <markkung08@gmail.com>
2019-08-27 18:09:41 +0700

    Setup ant.design

---
+++ config-overrides.js
@@ -0,0 +1,12 @@
+const { override, fixBabelImports, addLessLoader } = require('customize-cra')
+
+module.exports = override(
+  fixBabelImports('import', {
+    libraryName: 'antd',
+    libraryDirectory: 'es',
+    style: true
+  }),
+  addLessLoader({
+    javascriptEnabled: true
+  })
+)
