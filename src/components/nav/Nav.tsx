67292ae1e43982330725107fd3b037464071376b
win11905 <30414878+win11905@users.noreply.github.com>
2019-08-28 23:09:08 +0700

    :sparkles: add drawer & change Nav to styled-component

--- src/components/nav/Nav.tsx
+++ src/components/nav/Nav.tsx
@@ -1,64 +1,91 @@
 import React, { useState } from 'react'
 import { Link, withRouter } from 'react-router-dom'
-import { LeftMenu } from './LeftMenu'
-import { RightMenu } from './RightMenu'
+import styled from 'styled-components'
 import { Drawer, Button, Menu } from 'antd'
-import './app.css'
+
+const responsive = `(max-width: 767px)`
+
+const Navigator = styled.nav`
+  padding: 0 20px;
+  overflow: auto;
+  background-color: white;
+`
+
+const Logo = styled.div`
+  float: left;
+  & a {
+    display: inline-block;
+    font-size: 17px;
+    padding-top: 10px;
+    padding-bottom: 8px;
+    @media ${responsive} {
+      padding-top: 11px;
+    }
+  }
+`
+const LeftMenu = styled.div`
+  float: left;
+  @media ${responsive} {
+    display: none;
+  }
+`
+const RightMenu = styled.div`
+  float: right;
+  @media ${responsive} {
+    display: none;
+  }
+`
+
+const BarMenu = styled(Button)`
+  float: right;
+  margin: 8px;
+  display: none !important;
+  @media ${responsive} {
+    display: inline-block !important;
+  }
+`
 
 const _Nav = (props: any) => {
   const [visible, setVisibility] = useState(false)
-
   return (
-    <nav className="menuBar">
-      <div className="logo">
-        <Link to="/">programming</Link>
-      </div>
-      <div className="menuCon">
-        <div className="leftMenu">
-          <Menu mode="horizontal" selectedKeys={[props.location.pathname]}>
-            <Menu.Item key="/tasks">
-              <Link to="/tasks">Tasks</Link>
-            </Menu.Item>
-            <Menu.Item key="/learn">
-              <Link to="/learn">Learn</Link>
-            </Menu.Item>
-            <Menu.Item key="/forum">
-              <Link to="/forum">Forum</Link>
-            </Menu.Item>
-            <Menu.Item key="/exam">
-              <Link to="/exam">Exam</Link>
-            </Menu.Item>
-          </Menu>
-        </div>
-        <div className="rightMenu">
-          <Menu mode="horizontal" selectedKeys={[props.location.pathname]}>
-            <Menu.Item key="/login">
-              <Link to="/login">Login</Link>
-            </Menu.Item>
-            <Menu.Item key="/register">
-              <Link to="/register">Register</Link>
-            </Menu.Item>
-          </Menu>
-        </div>
-        <Button
-          className="barsMenu"
-          type="primary"
-          onClick={() => setVisibility(true)}
-        >
-          <span className="barsBtn"></span>
-        </Button>
-        <Drawer
-          title="Basic Drawer"
-          placement="right"
-          closable={false}
-          onClose={() => setVisibility(false)}
-          visible={visible}
-        >
-          <LeftMenu />
-          <RightMenu />
-        </Drawer>
-      </div>
-    </nav>
+    <Navigator>
+      <Logo>
+        <Link to="/">programming.in.th</Link>
+      </Logo>
+      <LeftMenu>
+        <Menu mode="horizontal" selectedKeys={[props.location.pathname]}>
+          <Menu.Item key="/tasks">
+            <Link to="/tasks">Tasks</Link>
+          </Menu.Item>
+          <Menu.Item key="/learn">
+            <Link to="/learn">Learn</Link>
+          </Menu.Item>
+          <Menu.Item key="/forum">
+            <Link to="/forum">Forum</Link>
+          </Menu.Item>
+          <Menu.Item key="/exam">
+            <Link to="/exam">Exam</Link>
+          </Menu.Item>
+        </Menu>
+      </LeftMenu>
+      <RightMenu>
+        <Menu mode="horizontal" selectedKeys={[props.location.pathname]}>
+          <Menu.Item key="/login">
+            <Link to="/login">Login</Link>
+          </Menu.Item>
+          <Menu.Item key="/register">
+            <Link to="/register">Register</Link>
+          </Menu.Item>
+        </Menu>
+      </RightMenu>
+      <BarMenu icon="menu" onClick={() => setVisibility(true)} />
+      <Drawer
+        title="Menu"
+        placement="top"
+        onClose={() => setVisibility(false)}
+        visible={visible}
+      ></Drawer>
+    </Navigator>
   )
 }
 
