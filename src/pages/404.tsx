e54f2b7eb7f96cd99c568cb6ac3489227ce7abcc
Macbook <macbook@Macbooks-MacBook-Pro.local>
2019-09-07 18:20:41 +0700

    :lipstick: edit 404 ui page

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -3,40 +3,19 @@ import React from 'react'
 
 /* React Util */
 import { Link } from 'react-router-dom'
-import { Button } from '@material-ui/core'
+import { Result, Button } from 'antd'
 
 export const NotFound = () => {
-  const style: { [name: string]: React.CSSProperties } = {
-    container: {
-      width: '100%',
-      height: 'calc(100vh - 83px - 83px + 56px)',
-      display: 'flex',
-      flexDirection: 'column',
-      justifyContent: 'center',
-      alignItems: 'center'
-    },
-    h1: {
-      fontSize: '32px',
-      color: 'var(--snack)'
-    },
-    button: {
-      fontSize: '18px',
-      margin: '25px 0',
-      padding: '5px 50px'
-    },
-    a: {
-      textDecoration: 'none',
-      color: '#ffffff'
-    }
-  }
   return (
-    <div id="root" style={style.container}>
-      <p style={style.h1}>Page not found</p>
-      <Link to="/" style={style.a}>
-        <Button variant="contained" color="primary" style={style.button}>
-          Return
-        </Button>
-      </Link>
-    </div>
+    <Result
+      status="404"
+      title="404"
+      subTitle="Sorry, the page you visited does not exist."
+      extra={
+        <Link to="/">
+          <Button type="primary">Back Home</Button>
+        </Link>
+      }
+    />
   )
 }
