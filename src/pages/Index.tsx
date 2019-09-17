89c8c35fa9528400859a9ef7c5b9f55bb9fa4799
win11905 <donothavemoney@gmail.com>
2019-09-17 18:09:48 +0700

    debug responsive

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,61 +1,10 @@
 /* React */
 import React from 'react'
 
-/* React Component */
-import { CustomCard } from '../components/Card'
-import { ForumListWrapper } from '../components/ForumListWrapper'
+import { withRouter } from 'react-router'
 
-/* Static */
-import styles from '../assets/css/card.module.css'
-
-export const Index: React.FunctionComponent = () => (
-  <React.Fragment>
-    <div id="root">
-      <div className={styles.wrapper}>
-        <CustomCard
-          title="Tasks"
-          icon="functions"
-          to="/tasks"
-          color="rgb(61, 90, 254)"
-          shadow="rgba(61, 90, 254, .625)"
-          linkCard
-        />
-        <CustomCard
-          title="Learn"
-          icon="school"
-          to="/learn"
-          color="rgb(29, 233, 182)"
-          shadow="rgba(29, 233, 182, .625)"
-          linkCard
-        />
-        <CustomCard
-          title="Users"
-          icon="account_box"
-          to="/users"
-          color="rgb(255, 23, 68)"
-          shadow="rgba(255, 23, 68, .625)"
-          linkCard
-        />
-      </div>
-      <div className={styles.wrapper}>
-        <CustomCard
-          title="Forum"
-          icon="forum"
-          to="/forum"
-          color="rgb(0, 229, 255)"
-          shadow="rgba(0, 229, 255, .625)"
-          linkCard
-        />
-        <CustomCard
-          title="Exams"
-          icon="featured_play_list"
-          to="/exam"
-          color="rgb(213, 0, 249)"
-          shadow="rgba(213, 0, 249, .625)"
-          linkCard
-        />
-      </div>
-      <ForumListWrapper />
-    </div>
-  </React.Fragment>
+export const _Index: React.FunctionComponent = () => (
+  <React.Fragment></React.Fragment>
 )
+
+export const Index = withRouter(_Index)
