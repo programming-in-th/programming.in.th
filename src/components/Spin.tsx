50d0d4ecd4dbeffae22dfa44d776d19831c1e4a4
iammarkps <markkung08@gmail.com>
2019-09-24 18:43:58 +0700

    New Spin component

--- src/components/Spin.tsx
+++ src/components/Spin.tsx
@@ -1,4 +1,5 @@
 import styled from 'styled-components'
+import { Spin } from 'antd'
 
 export const SpinWrapper = styled.div`
   width: 100%;
@@ -8,3 +9,9 @@ export const SpinWrapper = styled.div`
   justify-content: center;
   align-items: center;
 `
+
+export const CustomSpin = () => (
+  <SpinWrapper>
+    <Spin tip="Loading..." size="large" />
+  </SpinWrapper>
+)
