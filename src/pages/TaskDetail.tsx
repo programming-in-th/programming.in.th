bf6cadec6c357a5b6a1a14ea82e50ca9d5e1c7ed
win11905 <donothavemoney@gmail.com>
2019-09-25 20:59:21 +0700

    add blank component to taskDetail

--- src/pages/TaskDetail.tsx
+++ src/pages/TaskDetail.tsx
@@ -7,6 +7,8 @@ import { ThunkDispatch } from 'redux-thunk'
 import { AnyAction } from 'redux'
 import { ITask } from '../redux/types/task'
 import { SubmitPage } from '../components/tasks/Submit'
+import { Row, Col } from 'antd'
+import styled from 'styled-components'
 
 interface ITaskProps {
   task?: ITask
@@ -15,22 +17,38 @@ interface ITaskProps {
   onInitialLoad: (id: string) => void
 }
 
+const BlankComponent = (height: any) => {
+  const BlankWrapper = styled.div`
+    width: 100%;
+    ${height};
+    padding: 10px 20px;
+    box-shadow: 0px 2px 4px -1px rgba(0, 0, 0, 0.2),
+      0px 4px 5px 0px rgba(0, 0, 0, 0.14), 0px 1px 10px 0px rgba(0, 0, 0, 0.12);
+    margin-top: 20px;
+    background-color: white;
+  `
+  return <BlankWrapper />
+}
+
 export class TaskDetailComponent extends React.Component<ITaskProps> {
   componentDidMount() {
     this.props.onInitialLoad(this.props.match.params.id)
   }
   render() {
     return (
-      <React.Fragment>
-        {/* {this.props.status === 'LOADING' ? (
-          <div>LOADING</div>
-        ) : (
-          <div> */}
-        {/* <Button onClick={this.showError}>show Log</Button> */}
-        <SubmitPage problemID={this.props.match.params.id} />
-        {/* </div>
-        )} */}
-      </React.Fragment>
+      <Row gutter={16}>
+        <Col span={1} />
+        <Col span={16}>
+          <BlankComponent height="600px" />
+          <BlankComponent height="350px" />
+          {/* <SubmitPage problemID={this.props.match.params.id} /> */}
+        </Col>
+        <Col span={6}>
+          <BlankComponent height="300px" />
+          <BlankComponent height="250px" />
+        </Col>
+        <Col span={1} />
+      </Row>
     )
   }
 }
