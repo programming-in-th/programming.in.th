e693dc30ff36d1bdb0b84f7869fff1d2ce85d916
iammarkps <markkung08@gmail.com>
2019-10-13 23:26:04 +0700

    :bug: Fix rendering bug

--- src/components/Head.tsx
+++ src/components/Head.tsx
@@ -26,31 +26,17 @@ export const Head: React.FunctionComponent<{}> = () => (
       property="og:description"
       content="Programming.in.th provides you with the fundamentals of algorithmic problem-solving, an important skill to differentiate yourself as a programmer in an increasingly technologically advanced world."
     />
+    <link
+      rel="stylesheet"
+      href="https://cdn.jsdelivr.net/npm/katex@0.11.0/dist/katex.min.css"
+      integrity="sha384-BdGj8xC2eZkQaxoQ8nSLefg4AV4/AwB3Fj+8SUSo7pnKP6Eoy18liIKTPn9oBYNG"
+      crossOrigin="anonymous"
+    />
     <meta property="og:image" content="assets/img/og.jpg" />
     <meta property="og:image:type" content="image/jpeg" />
     <meta property="og:image:width" content="1200" />
     <meta property="og:image:height" content="630" />
     <meta name="twitter:card" content="summary" />
-    <style type="text/css">
-      {` @font-face {
-        font-family: 'Sarabun';
-        font-style: normal;
-        font-weight: 400;
-        font-display: swap;
-        src: local('Sarabun Regular'), local('Sarabun-Regular'),
-          url('assets/fonts/THSarabun.woff2') format('woff2');
-        unicode-range: U+0E01-0E5B, U+200C-200D, U+25CC;
-      }
-
-      @font-face {
-        font-family: 'Kanit';
-        font-style: normal;
-        font-weight: 400;
-        font-display: swap;
-        src: url('assets/fonts/Kanit-Regular.woff2') format('woff2');
-        unicode-range: U+0E01-0E5B, U+200C-200D, U+25CC;
-      }`}
-    </style>
     <title>programming.in.th | Beta</title>
   </Helmet>
 )
