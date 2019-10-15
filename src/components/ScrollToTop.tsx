7add83bf9008d6b9165ce1f447bd488a0598fe6c
win11905 <donothavemoney@gmail.com>
2019-10-15 11:15:55 +0700

    :zap: add type to scrollToTop

--- src/components/ScrollToTop.tsx
+++ src/components/ScrollToTop.tsx
@@ -1,8 +1,9 @@
 import React from 'react'
 import { withRouter } from 'react-router-dom'
+import { RouteComponentProps } from 'react-router'
 
-class Main extends React.Component<any> {
-  componentDidUpdate(prevProps: any) {
+class Main extends React.Component<RouteComponentProps> {
+  componentDidUpdate(prevProps: RouteComponentProps) {
     if (this.props.location.pathname !== prevProps.location.pathname) {
       window.scrollTo(0, 0)
     }
