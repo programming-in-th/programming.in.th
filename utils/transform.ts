da668efe829f3bbd4b1d93237e80a97865dff13d
win11905 <donothavemoney@gmail.com>
2019-10-22 11:16:14 +0700

    :sparkles: add submission page

---
+++ utils/transform.ts
@@ -0,0 +1,31 @@
+import { ISubmission } from '../redux/types/submission'
+import firebase from '../lib/firebase'
+
+export const transformStatus = (detail: ISubmission) => {
+  if (detail === undefined) return
+
+  if (detail.status === 'in_queue') {
+    return Object.assign({}, detail, {
+      status: 'PENDING',
+      points: 'PENDING',
+      memory: 0,
+      time: 0
+    })
+  }
+
+  return detail
+}
+
+export interface ITimeObject {
+  _seconds: number
+  _nanoseconds: number
+}
+
+export const transformDate = (time: ITimeObject) => {
+  const firebaseDate = new firebase.firestore.Timestamp(
+    time._seconds,
+    time._nanoseconds
+  )
+  const jsDate = firebaseDate.toDate()
+  return jsDate.toLocaleString()
+}
