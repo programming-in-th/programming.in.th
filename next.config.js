2feb42e8be2b2c4a5c955b52d852f061c68c8a10
iammarkps <markkung08@gmail.com>
2019-11-02 18:24:44 +0700

    :zap: Load antd css ondemand

--- next.config.js
+++ next.config.js
@@ -2,6 +2,7 @@ const withCss = require('@zeit/next-css')
 const remarkMath = require('remark-math')
 const rehypeKatex = require('rehype-katex')
 const rehypePrism = require('@mapbox/rehype-prism')
+const withPlugins = require('next-compose-plugins')
 
 const withMDX = require('@zeit/next-mdx')({
   extension: /\.mdx?$/,
@@ -11,8 +12,39 @@ const withMDX = require('@zeit/next-mdx')({
   }
 })
 
-module.exports = withCss(
-  withMDX({
-    pageExtensions: ['tsx', 'mdx']
-  })
+module.exports = withPlugins(
+  [
+    withCss,
+    [
+      withMDX,
+      {
+        pageExtensions: ['tsx', 'mdx']
+      }
+    ]
+  ],
+  {
+    webpack: (config, { isServer }) => {
+      if (isServer) {
+        const antStyles = /antd\/.*?\/style\/css.*?/
+        const origExternals = [...config.externals]
+        config.externals = [
+          (context, request, callback) => {
+            if (request.match(antStyles)) return callback()
+            if (typeof origExternals[0] === 'function') {
+              origExternals[0](context, request, callback)
+            } else {
+              callback()
+            }
+          },
+          ...(typeof origExternals[0] === 'function' ? [] : origExternals)
+        ]
+
+        config.module.rules.unshift({
+          test: antStyles,
+          use: 'null-loader'
+        })
+      }
+      return config
+    }
+  }
 )
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,7 +2,6 @@ import App, { AppProps } from 'next/app'
 import React from 'react'
 import { Provider } from 'react-redux'
 import NProgress from 'nprogress'
-import 'antd/dist/antd.css'
 
 import Router from 'next/router'
 import { initStore } from '../redux'
