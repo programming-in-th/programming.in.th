84fcbb06186a765c5341408799f3ef3616de9484
iammarkps <markkung08@gmail.com>
2019-11-02 20:06:14 +0700

    :mag: Improve SEO

--- src/components/head/index.tsx
+++ src/components/head/index.tsx
@@ -5,6 +5,27 @@ import { Fonts } from './fonts'
 export const CustomHead = () => (
   <Head>
     <Fonts />
+    <meta charSet="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <meta name="theme-color" content="#000000" />
+    <link rel="icon" sizes="192x192" href="/assets/img/icon-512.png" />
+    <link rel="apple-touch-icon" href="/assets/img/icon-ios" />
+    <meta
+      name="Description"
+      content="Programming.in.th provides you with the fundamentals of algorithmic problem-solving, an important skill to differentiate yourself as a programmer in an increasingly technologically advanced world."
+    />
+    <meta property="og:url" content="https://staging.programming.in.th" />
+    <meta property="og:type" content="website" />
+    <meta property="og:title" content="programming.in.th" />
+    <meta
+      property="og:description"
+      content="Programming.in.th provides you with the fundamentals of algorithmic problem-solving, an important skill to differentiate yourself as a programmer in an increasingly technologically advanced world."
+    />
+    <meta property="og:image" content="/assets/img/og.jpg" />
+    <meta property="og:image:type" content="image/jpeg" />
+    <meta property="og:image:width" content="1200" />
+    <meta property="og:image:height" content="630" />
+    <meta name="twitter:card" content="summary" />
     <link
       rel="preload"
       href="https://cdn.jsdelivr.net/npm/katex@0.11.1/dist/katex.min.css"
