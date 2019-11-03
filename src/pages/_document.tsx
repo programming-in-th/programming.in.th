05cece8b2fb32046f829da12aaf80e774e91476c
iammarkps <markkung08@gmail.com>
2019-11-03 12:30:25 +0700

    :mag: Move tag to _document

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -6,6 +6,7 @@ import Document, {
   NextScript
 } from 'next/document'
 import { ServerStyleSheet } from 'styled-components'
+import { CustomHead } from '../components/head'
 
 export default class MyDocument extends Document {
   static async getInitialProps(ctx: DocumentContext) {
@@ -36,7 +37,9 @@ export default class MyDocument extends Document {
   render() {
     return (
       <Html lang="en">
-        <Head />
+        <Head>
+          <CustomHead />
+        </Head>
         <body>
           <Main />
           <NextScript />
