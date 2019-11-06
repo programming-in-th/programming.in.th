3fdc24afdd100f16bdfe7ca5786a60e5d5fc1acf
win11905 <donothavemoney@gmail.com>
2019-11-06 17:18:33 +0700

    :sparkles: add admin layout

---
+++ src/components/admin/AdminLayout.tsx
@@ -0,0 +1,82 @@
+import React from 'react'
+import { useSelector } from 'react-redux'
+import { PageLayout } from '../Layout'
+import { NotAdmin } from './Already'
+import { IAppState } from '../../redux'
+import { Menu } from 'antd'
+import styled from 'styled-components'
+import { media } from '../../design/Responsive'
+import Link from 'next/link'
+import { useRouter } from 'next/router'
+
+const SettingLayoutWrapper = styled.div`
+  margin: 64px;
+
+  ${media('TABLET')} {
+    margin: 8px;
+  }
+`
+
+const SettingLayout = styled.div`
+  display: flex;
+  background-color: #ffffff;
+  display: flex;
+  width: 100%;
+  height: 100%;
+  padding-top: 16px;
+  padding-bottom: 16px;
+  overflow: auto;
+
+  ${media('TABLET')} {
+    padding: 0;
+    flex-direction: column;
+  }
+`
+
+const Left = styled.div`
+  width: 224px;
+  border-right: 1px solid #e8e8e8;
+
+  ${media('TABLET')} {
+    width: 100%;
+    border: none;
+  }
+`
+
+const Right = styled.div`
+  padding: 5px;
+  width: 100%;
+`
+
+export const AdminLayout: React.FunctionComponent = props => {
+  const isAdmin = useSelector((state: IAppState) => state.user.admin)
+  const router = useRouter()
+  const path = router.pathname
+
+  if (!isAdmin)
+    return (
+      <PageLayout>
+        <NotAdmin />
+      </PageLayout>
+    )
+
+  return (
+    <PageLayout>
+      <SettingLayoutWrapper>
+        <SettingLayout>
+          <Left>
+            <Menu mode="inline" selectedKeys={[path]}>
+              <Menu.Item key="/admin/user">
+                <Link href="/admin/user">User</Link>
+              </Menu.Item>
+              <Menu.Item key="/admin/task">
+                <Link href="/admin/task">Task</Link>
+              </Menu.Item>
+            </Menu>
+          </Left>
+          <Right>{props.children}</Right>
+        </SettingLayout>
+      </SettingLayoutWrapper>
+    </PageLayout>
+  )
+}
--- src/pages/admin/index.tsx
+++ src/pages/admin/index.tsx
@@ -1,66 +1,6 @@
-import React, { useState, useEffect } from 'react'
-import { useSelector, useDispatch } from 'react-redux'
-import { PageLayout } from '../../components/Layout'
-import styled, { keyframes } from 'styled-components'
-import { IAppState } from '../../redux'
-import { NotAdmin } from '../../components/admin/Already'
-import firebase from '../../lib/firebase'
-import useSWR from 'swr'
-import { WhiteContainerWrapper } from '../../design/Atomics'
-import { fetchUserList } from '../../utils/fetchUserList'
-import { Table, Switch, Icon } from 'antd'
+import React from 'react'
+import { AdminLayout } from '../../components/admin/AdminLayout'
 
 export default () => {
-  const isAdmin = useSelector((state: IAppState) => state.user.admin)
-  const { data } = useSWR('getAllUser', fetchUserList)
-  const [state, setState] = useState('')
-  const columns = [
-    {
-      title: 'Display Name',
-      dataIndex: 'displayName'
-    },
-    {
-      title: 'Email',
-      dataIndex: 'email'
-    },
-    {
-      title: 'Admin',
-      dataIndex: 'admin',
-      render: (isadmin: boolean, row: any) => (
-        <span>
-          <Switch
-            checkedChildren={<Icon type="check" />}
-            unCheckedChildren={<Icon type="close" />}
-            defaultChecked={isadmin}
-            loading={row.uid === state}
-            onChange={async (checked: boolean) => {
-              setState(row.uid)
-              const param = { uid: row.uid, checked }
-              const response = await firebase
-                .app()
-                .functions('asia-east2')
-                .httpsCallable('updateAdmin')(param)
-              if (response.data) setState('')
-              else console.log('ERROR')
-            }}
-          />
-        </span>
-      )
-    }
-  ] as any
-
-  return (
-    <PageLayout>
-      <WhiteContainerWrapper>
-        {data ? (
-          <Table
-            columns={columns}
-            dataSource={data.data}
-            size="middle"
-            pagination={{ pageSize: 1000 }}
-          />
-        ) : null}
-      </WhiteContainerWrapper>
-    </PageLayout>
-  )
+  return <AdminLayout />
 }
