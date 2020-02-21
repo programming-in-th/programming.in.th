9524519deb7968d496f960c2f2b5f155f4bca17e
iammarkps <markkung08@gmail.com>
2020-02-21 22:13:17 +0700

    :fire: Remove dead code

--- src/components/learn/elements/Img.tsx
+++ src/components/learn/elements/Img.tsx
@@ -1,8 +1,5 @@
-import styled from 'styled-components'
 import { SimpleImg, Props as SimpleImgProps } from 'react-simple-img'
 
-const Image = (props: SimpleImgProps) => <SimpleImg height="100%" {...props} />
-
-export const Img = styled(Image)`
-  max-width: 100%;
-`
+export const Img = (props: SimpleImgProps) => (
+  <SimpleImg imgStyle={{ maxWidth: '100%', objectFit: 'contain' }} {...props} />
+)
