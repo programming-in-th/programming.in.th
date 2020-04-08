4f83813d5744facc4c8909128efb00d234beeb2d
iammarkps <markkung08@gmail.com>
2020-04-08 21:59:31 +0700

    :label:

--- index.d.ts
+++ index.d.ts
@@ -7,3 +7,7 @@ declare module '*.svg' {
   const content: any
   export default content
 }
+
+declare interface Window {
+  google: any
+}
--- src/components/auth/onetap.ts
+++ src/components/auth/onetap.ts
@@ -4,16 +4,18 @@ const PUBLIC_CLIENT_ID =
   '408883593148-sm3fup3jg8b932h1lolml6cklu8ntlko.apps.googleusercontent.com'
 
 const callback = async res => {
-  const FirebaseCredential = await firebase.auth.GoogleAuthProvider.credential(
+  const FirebaseCredential = firebase.auth.GoogleAuthProvider.credential(
     res.credential
   )
+
   await firebase.auth().signInWithCredential(FirebaseCredential)
 }
 
 export const onetap = () => {
-  ;(window as any).google.accounts.id.initialize({
+  window.google.accounts.id.initialize({
     client_id: PUBLIC_CLIENT_ID,
     callback: callback
   })
-  ;(window as any).google.accounts.id.prompt()
+
+  window.google.accounts.id.prompt()
 }
