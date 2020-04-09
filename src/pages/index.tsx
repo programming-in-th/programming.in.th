939288bd249e31dcff6bcdb7517e584230905a11
iammarkps <markkung08@gmail.com>
2020-04-10 04:10:04 +0700

    :lipstick: Improve placeholder UI

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,7 +6,7 @@ import { PageLayout } from '../components/Layout'
 export default () => (
   <PageLayout>
     <Flex align="center" justify="center" width="100%">
-      <Text fontSize={['4xl', '6xl']} fontWeight="800" textAlign="center">
+      <Text fontSize={['2xl', '6xl']} fontWeight="800" textAlign="center">
         PROGRAMMING.IN.TH
       </Text>
     </Flex>
