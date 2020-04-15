71f461ebae84b7ea642382c09c4ce71b3a868633
iammarkps <markkung08@gmail.com>
2020-04-16 00:04:05 +0700

    :lipstick:

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,7 +5,7 @@ import { PageLayout } from '../components/Layout'
 
 export default () => (
   <PageLayout>
-    <Flex align="center" justify="center" width="100%">
+    <Flex align="center" justify="center" flexGrow={1}>
       <Text fontSize={['2xl', '6xl']} fontWeight="800" textAlign="center">
         PROGRAMMING.IN.TH
       </Text>
--- src/pages/tasks/index.tsx
+++ src/pages/tasks/index.tsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import { Text, Flex } from '@chakra-ui/core'
 
-import { PageLayout } from '../components/Layout'
+import { PageLayout } from '../../components/Layout'
 
 export default () => (
   <PageLayout>
-    <Flex align="center" justify="center" width="100%">
+    <Flex align="center" justify="center" flexGrow={1}>
       <Text fontSize="6xl" fontWeight="800" textAlign="center">
         TASKS
       </Text>
