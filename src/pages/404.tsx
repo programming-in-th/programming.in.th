03f1cb9e2e07460711da8fc83a6a4338320bd1a7
iammarkps <markkung08@gmail.com>
2020-04-28 14:30:15 +0700

    :lipstick: Fix 404 centering

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -5,8 +5,8 @@ import { PageLayout } from '../components/Layout'
 
 export default () => (
   <PageLayout>
-    <Flex align="center" justify="center" width="100%">
-      <Text fontSize={['4xl', '6xl']} fontWeight="600" textAlign="center">
+    <Flex align="center" justify="center" flexGrow={1}>
+      <Text fontSize="6xl" fontWeight="800" textAlign="center">
         404 | Not Found
       </Text>
     </Flex>
