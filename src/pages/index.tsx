f4d4868dd0f733c77d9a3de27ad974f8a26239c6
iammarkps <markkung08@gmail.com>
2020-04-30 02:56:17 +0700

    :lipstick:

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,14 @@
 import React from 'react'
-import { Text, Flex } from '@chakra-ui/core'
+import { Flex, Heading } from '@chakra-ui/core'
 
 import { PageLayout } from '../components/Layout'
 
 export default () => (
   <PageLayout>
     <Flex align="center" justify="center" flexGrow={1}>
-      <Text fontSize={['2xl', '6xl']} fontWeight="800" textAlign="center">
+      <Heading fontSize={['2xl', '6xl']} fontWeight="800" textAlign="center">
         PROGRAMMING.IN.TH
-      </Text>
+      </Heading>
     </Flex>
   </PageLayout>
 )
