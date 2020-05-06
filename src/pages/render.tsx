bd635c7b0299b5d7349db9f5c2cce234f3ed4bf2
win11905 <donothavemoney@gmail.com>
2020-05-06 23:24:29 +0700

    :lipstick:

--- src/pages/render.tsx
+++ src/pages/render.tsx
@@ -8,45 +8,49 @@ import { saveAs } from 'file-saver'
 export default () => {
   const [solution, setSolution] = useState<string>('')
   const [input, setInput] = useState<string>('')
-  useEffect(() => {
+  const refetch = () => {
     try {
       const sol = renderMarkdown(input)
       setSolution(sol)
     } catch (err) {
       setSolution('Markdown Rendering Error')
     }
-  }, [input])
+  }
 
-  const handleClick = () => {
+  const genMarkdownFile = () => {
     var blob = new Blob([input], { type: 'text/plain;charset=utf-8' })
     saveAs(blob, 'output.md')
   }
 
   return (
     <PageLayout>
-      <Heading mx="auto" my={4}>
-        Rendering
-      </Heading>
+      <Flex direction={['column', 'row']} justify="space-between" mx={16}>
+        <Box w="100%" my={4}>
+          <Heading>Rendering</Heading>
+        </Box>
+        <Box w="100%" mt={8}>
+          <Button onClick={refetch}>Compile</Button>
+        </Box>
+      </Flex>
       <Flex direction={['column', 'row']} height="100%" flexGrow={1} mx={10}>
         <Textarea
           onChange={(event) => setInput(event.target.value)}
           h="calc(100vh - 230px)"
           w="100%"
-          mr={4}
         />
         <Box
-          minW={['100%', 800]}
-          maxHeight="calc(100vh - 230px)"
+          minW={['100%', 830]}
+          maxHeight={['100%', 'calc(100vh - 230px)']}
           overflow="scroll"
         >
-          {input ? (
+          {solution ? (
             <Solution solution={solution} />
           ) : (
             <Solution solution="Solution will show up here" />
           )}
         </Box>
       </Flex>
-      <Button onClick={handleClick} borderRadius="0" h={20} fontSize="lg">
+      <Button onClick={genMarkdownFile} borderRadius="0" h={20} fontSize="lg">
         Save Markdown
       </Button>
     </PageLayout>
