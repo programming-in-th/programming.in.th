ee0ffba59b5b51225a4192cb4d807791c925e132
Sirawit Pongnakintr <plurm2545@hotmail.com>
2020-05-06 18:01:25 +0700

    :zap: add error catching and download button (#352)
    * add error catching and download button
    
    * remove unused import
    
    Co-authored-by: Plum <sirawit@brikl.com>

--- src/pages/render.tsx
+++ src/pages/render.tsx
@@ -1,16 +1,26 @@
 import React, { useState, useEffect } from 'react'
 import { renderMarkdown } from '../utils/renderMarkdown'
-import { Flex, Textarea, Box, Heading } from '@chakra-ui/core'
+import { Flex, Textarea, Box, Heading, Button } from '@chakra-ui/core'
 import { PageLayout } from '../components/Layout'
 import { Solution } from '../components/tasks/Solution'
+import { saveAs } from 'file-saver'
 
 export default () => {
   const [solution, setSolution] = useState<string>('')
   const [input, setInput] = useState<string>('')
   useEffect(() => {
-    const sol = renderMarkdown(input)
-    setSolution(sol)
+    try {
+      const sol = renderMarkdown(input)
+      setSolution(sol)
+    } catch (err) {
+      setSolution('Markdown Rendering Error')
+    }
   }, [input])
+
+  const handleClick = () => {
+    var blob = new Blob([input], { type: 'text/plain;charset=utf-8' })
+    saveAs(blob, 'output.md')
+  }
 
   return (
     <PageLayout>
@@ -20,9 +30,9 @@ export default () => {
       <Flex direction={['column', 'row']} height="100%" flexGrow={1} mx={6}>
         <Textarea
           onChange={(event) => setInput(event.target.value)}
-          h="700px"
+          minHeight="700px"
           w="100%"
-          mx={10}
+          mx={[0, 10]}
         />
         <Box w={['100%', 800]}>
           {input ? (
@@ -32,6 +42,9 @@ export default () => {
           )}
         </Box>
       </Flex>
+      <Button onClick={handleClick} borderRadius="0" variantColor="green">
+        Save Markdown
+      </Button>
     </PageLayout>
   )
 }
