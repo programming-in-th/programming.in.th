1787101772913b5eb603209d6e2dcc4f547b4969
win11905 <donothavemoney@gmail.com>
2020-05-13 04:40:24 +0700

    :zap: use onSnapShot instead of useSWR

--- src/components/UserContext.tsx
+++ src/components/UserContext.tsx
@@ -4,41 +4,45 @@ export type UserState = typeof initialState
 
 type ITheme = 'material' | 'monokai' | 'solarized'
 
-interface IContext {
+export type IUser = {
   user: firebase.User | null | undefined
+}
+export interface IContext {
   displayName: string
   admin: boolean
   codeTheme: ITheme
 }
 
-export const initialState: IContext = {
+export type IUserContext = IUser & IContext
+
+export const initialState: IUserContext = {
   user: undefined,
   displayName: '',
   admin: false,
-  codeTheme: 'material'
+  codeTheme: 'material',
 }
 
 export type UserAction =
   | {
       type: 'RECEIVE_USER'
-      payload: { user: firebase.User | null }
+      payload: IUser
     }
   | {
       type: 'RECEIVE_CONTEXT'
-      payload: { displayName: string; admin: boolean; codeTheme: ITheme }
+      payload: IContext
     }
 
 export const reducer = (state: UserState, action: UserAction): UserState => {
   switch (action.type) {
     case 'RECEIVE_USER':
       return Object.assign({}, state, {
-        user: action.payload.user
+        user: action.payload.user,
       })
     case 'RECEIVE_CONTEXT':
       return Object.assign({}, state, {
         displayName: action.payload.displayName,
         admin: action.payload.admin,
-        codeTheme: action.payload.codeTheme
+        codeTheme: action.payload.codeTheme,
       })
     default:
       return state
