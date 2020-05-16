7e74cec5cdb7c6790ba8604c976b3c790603e07d
iammarkps <markkung08@gmail.com>
2020-05-17 00:06:29 +0700

    :fire:

--- next.config.js
+++ next.config.js
@@ -30,18 +30,6 @@ module.exports = withPlugins(
     withOffline,
   ],
   {
-    analyzeServer: ['server', 'both'].includes(process.env.BUNDLE_ANALYZE),
-    analyzeBrowser: ['browser', 'both'].includes(process.env.BUNDLE_ANALYZE),
-    bundleAnalyzerConfig: {
-      server: {
-        analyzerMode: 'static',
-        reportFilename: '../bundles/server.html',
-      },
-      browser: {
-        analyzerMode: 'static',
-        reportFilename: '../bundles/client.html',
-      },
-    },
     transformManifest: (manifest) => ['/'].concat(manifest),
     workboxOpts: {
       swDest: 'static/service-worker.js',
