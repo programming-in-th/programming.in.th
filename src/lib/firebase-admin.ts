1d31bce7f918b1e5ed9a82abc8260d0f5275ff57
iammarkps <markkung08@gmail.com>
2020-05-22 04:06:26 +0700

    :bug: Fix PEM encode format

--- src/lib/firebase-admin.ts
+++ src/lib/firebase-admin.ts
@@ -8,7 +8,7 @@ export default function getDb() {
       credential: admin.credential.cert({
         projectId: process.env.FIREBASE_PROJECT_ID,
         clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-        privateKey: process.env.FIREBASE_PRIVATE_KEY,
+        privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
       }),
       databaseURL: 'https://proginth.firebaseio.com',
     })
