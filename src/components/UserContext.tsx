825a26d155b4361ee84774be2bf2c01cd4c544ef
Possawat Suksai <markkung08@gmail.com>
2020-05-22 21:36:15 +0700

    Refactor auth (#382)
    * :sparkles: Migrate to direct database query
    
    * :bug: Fix PEM encode format
    
    * :bug: Replace \n with real line break
    
    * :recycle: Refactor user to other file
    
    * :bug: Fix wrong firebase import location
    
    * :recycle:

--- src/components/UserContext.tsx
+++ src/components/UserContext.tsx
@@ -1,26 +1,31 @@
-import React, { useContext } from 'react'
+import React, { useContext, useReducer, useEffect } from 'react'
+import useSWR, { mutate } from 'swr'
+import { fetchFromFirebase } from 'utils/fetcher'
+import firebase from 'lib/firebase'
+import { onetap } from './auth/onetap'
 
 type Theme = 'material' | 'monokai' | 'solarized'
 
-export type User = {
+type User = {
   user: firebase.User | null | undefined
 }
-export interface Context {
+interface Context {
   username: string
   admin: boolean
   codeTheme: Theme
 }
 
-export type UserState = User & Context
+type UserState = User & Context & { loading: boolean }
 
-export const initialState: UserState = {
+const initialState: UserState = {
   user: undefined,
   username: '',
   admin: false,
   codeTheme: 'material',
+  loading: true,
 }
 
-export type UserAction =
+type UserAction =
   | {
       type: 'RECEIVE_USER'
       payload: User
@@ -29,8 +34,11 @@ export type UserAction =
       type: 'RECEIVE_CONTEXT'
       payload: Context
     }
+  | {
+      type: 'LOADING_DONE'
+    }
 
-export const reducer = (state: UserState, action: UserAction): UserState => {
+const reducer = (state: UserState, action: UserAction): UserState => {
   switch (action.type) {
     case 'RECEIVE_USER':
       return Object.assign({}, state, {
@@ -42,11 +50,67 @@ export const reducer = (state: UserState, action: UserAction): UserState => {
         admin: action.payload.admin,
         codeTheme: action.payload.codeTheme,
       })
+    case 'LOADING_DONE':
+      return Object.assign({}, state, {
+        loading: false,
+      })
     default:
       return state
   }
 }
 
-export const UserStateContext = React.createContext<UserState>(initialState)
+const UserStateContext = React.createContext<UserState>(initialState)
 
 export const useUser = () => useContext(UserStateContext)
+
+const userContextComp = ({ children }) => {
+  const [userState, userDispatch] = useReducer<
+    React.Reducer<UserState, UserAction>
+  >(reducer, initialState)
+
+  const { data: userContext } = useSWR('getUserContext', fetchFromFirebase, {
+    refreshInterval: 1000 * 60,
+  })
+
+  useEffect(() => {
+    const unsubscribe = firebase.auth().onAuthStateChanged((user) => {
+      if (user === null || user.emailVerified) {
+        userDispatch({
+          type: 'RECEIVE_USER',
+          payload: {
+            user,
+          },
+        })
+
+        mutate('getUserContext')
+      }
+    })
+
+    return () => {
+      unsubscribe()
+    }
+  }, [])
+
+  useEffect(() => {
+    if (userContext) {
+      userDispatch({
+        type: 'RECEIVE_CONTEXT',
+        payload: userContext,
+      })
+    }
+  }, [userContext])
+
+  useEffect(() => {
+    if (userState.user === null) {
+      onetap()
+    }
+  }, [userState.user])
+
+  return (
+    <UserStateContext.Provider value={userState}>
+      {children}
+    </UserStateContext.Provider>
+  )
+}
+
+export default userContextComp
