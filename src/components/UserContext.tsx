e8781f25a1374f363bd4ae6e87f2a669ba00f767
iammarkps <markkung08@gmail.com>
2020-05-22 20:40:02 +0700

    :recycle: Refactor user to other file

--- src/components/UserContext.tsx
+++ src/components/UserContext.tsx
@@ -1,4 +1,8 @@
-import React, { useContext } from 'react'
+import React, { useContext, useReducer, useEffect } from 'react'
+import useSWR, { mutate } from 'swr'
+import { fetchFromFirebase } from 'utils/fetcher'
+import firebase from 'firebase'
+import { onetap } from './auth/onetap'
 
 type Theme = 'material' | 'monokai' | 'solarized'
 
@@ -11,13 +15,14 @@ export interface Context {
   codeTheme: Theme
 }
 
-export type UserState = User & Context
+export type UserState = User & Context & { loading: boolean }
 
 export const initialState: UserState = {
   user: undefined,
   username: '',
   admin: false,
   codeTheme: 'material',
+  loading: true,
 }
 
 export type UserAction =
@@ -28,6 +33,9 @@ export type UserAction =
   | {
       type: 'RECEIVE_CONTEXT'
       payload: Context
+    }
+  | {
+      type: 'LOADING_DONE'
     }
 
 export const reducer = (state: UserState, action: UserAction): UserState => {
@@ -42,6 +50,10 @@ export const reducer = (state: UserState, action: UserAction): UserState => {
         admin: action.payload.admin,
         codeTheme: action.payload.codeTheme,
       })
+    case 'LOADING_DONE':
+      return Object.assign({}, state, {
+        loading: false,
+      })
     default:
       return state
   }
@@ -50,3 +62,53 @@ export const reducer = (state: UserState, action: UserAction): UserState => {
 export const UserStateContext = React.createContext<UserState>(initialState)
 
 export const useUser = () => useContext(UserStateContext)
+
+export default function UserContextComp({ children }) {
+  const [userState, userDispatch] = useReducer<
+    React.Reducer<UserState, UserAction>
+  >(reducer, initialState)
+
+  const { data: userContext } = useSWR('getUserContext', fetchFromFirebase, {
+    refreshInterval: 1000 * 60,
+  })
+
+  useEffect(() => {
+    const unsubscribe = firebase.auth().onAuthStateChanged((user) => {
+      if (user === null || user.emailVerified) {
+        userDispatch({
+          type: 'RECEIVE_USER',
+          payload: {
+            user,
+          },
+        })
+
+        mutate('getUserContext')
+      }
+    })
+
+    return () => {
+      unsubscribe()
+    }
+  }, [])
+
+  useEffect(() => {
+    if (userContext) {
+      userDispatch({
+        type: 'RECEIVE_CONTEXT',
+        payload: userContext,
+      })
+    }
+  }, [userContext])
+
+  useEffect(() => {
+    if (userState.user === null) {
+      onetap()
+    }
+  }, [userState.user])
+
+  return (
+    <UserStateContext.Provider value={userState}>
+      {children}
+    </UserStateContext.Provider>
+  )
+}
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,26 +1,15 @@
 import { NextPage } from 'next'
 import { AppProps } from 'next/app'
 import Router from 'next/router'
-import React, { useReducer, useEffect } from 'react'
-import useSWR, { mutate } from 'swr'
+import React from 'react'
 import NProgress from 'nprogress'
 import { ThemeProvider, CSSReset } from '@chakra-ui/core'
-
-import firebase from 'lib/firebase'
-import {
-  UserState,
-  initialState,
-  reducer,
-  UserAction,
-  UserStateContext,
-} from 'components/UserContext'
-import { onetap } from 'components/auth/onetap'
+import UserProvider from 'components/UserContext'
 
 import { GlobalStyle } from 'design'
 import { customTheme } from 'design/theme'
 
 import 'assets/css/prism.css'
-import { fetchFromFirebase } from 'utils/fetcher'
 
 let timeout: any
 
@@ -38,55 +27,13 @@ Router.events.on('routeChangeComplete', done)
 Router.events.on('routeChangeError', done)
 
 const App: NextPage<AppProps> = ({ Component, pageProps }) => {
-  const [userState, userDispatch] = useReducer<
-    React.Reducer<UserState, UserAction>
-  >(reducer, initialState)
-
-  const { data: userContext } = useSWR('getUserContext', fetchFromFirebase, {
-    refreshInterval: 1000 * 60,
-  })
-
-  useEffect(() => {
-    const unsubscribe = firebase.auth().onAuthStateChanged((user) => {
-      if (user === null || user.emailVerified) {
-        userDispatch({
-          type: 'RECEIVE_USER',
-          payload: {
-            user,
-          },
-        })
-
-        mutate('getUserContext')
-      }
-    })
-
-    return () => {
-      unsubscribe()
-    }
-  }, [])
-
-  useEffect(() => {
-    if (userContext) {
-      userDispatch({
-        type: 'RECEIVE_CONTEXT',
-        payload: userContext,
-      })
-    }
-  }, [userContext])
-
-  useEffect(() => {
-    if (userState.user === null) {
-      onetap()
-    }
-  }, [userState.user])
-
   return (
     <ThemeProvider theme={customTheme}>
-      <UserStateContext.Provider value={userState}>
+      <UserProvider>
         <CSSReset />
         <GlobalStyle />
         <Component {...pageProps} />
-      </UserStateContext.Provider>
+      </UserProvider>
     </ThemeProvider>
   )
 }
