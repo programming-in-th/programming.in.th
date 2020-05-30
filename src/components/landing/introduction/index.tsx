1cdbe15cdbfdafe157995b03b6b29a3677afb2d0
SaltyAom <aomkirby@mystiar.com>
2020-05-30 13:58:03 +0700

    :tada: Mock intl

--- src/components/landing/introduction/index.tsx
+++ src/components/landing/introduction/index.tsx
@@ -2,44 +2,59 @@ import Link from 'next/link'
 
 import './landing-introduction.styl'
 
-const Introduction = () => (
-  <section id="landing-introduction">
-    <article className="article">
-      <h1 className="brand">Programming.in.th</h1>
-      <h2 className="title">
-        It's time to
-        <span className="keyword">Level Up</span>
-      </h2>
-      <ul
-        className="unorder"
-        style={{ listStyleImage: 'url(/icon/check.svg)' }}
-      >
-        <li className="list">Data Structure & Algorithms</li>
-        <li className="list">Over 400 Challenges</li>
-        <li className="list">Join over 10,000 programmers.</li>
-      </ul>
-      <footer className="action">
-        <Link href="/dashboard">
-          <a className="enroll">
-            Start today
-            <img className="icon" src="/icon/chevron_right.svg" alt="Chevron" />
-          </a>
-        </Link>
-        <Link href="/tasks">
-          <a className="explore">Explore Challenge</a>
-        </Link>
-      </footer>
-      {/* <a className="provider">Get start now, it's free.</a> */}
-    </article>
-    <figure className="cover">
-      <picture className="container">
-        <img
-          className="image"
-          src="https://vignette.wikia.nocookie.net/himoto-umaruchan/images/a/a2/Umaru%27s_anime_design_%28chibi%29.png/revision/latest?cb=20200411195915"
-        />
-      </picture>
-    </figure>
-  </section>
-)
+import intl from '../../../intl/index.json'
+
+const Introduction = () => {
+  // Transform this to state management engine.
+  const lang = 'en'
+
+  const translated = intl[lang]
+
+  const { header, list, action, subAction } = translated.introduction
+
+  return (
+    <section id="landing-introduction">
+      <article className="article">
+        <h1 className="brand">Programming.in.th</h1>
+        <h2 className="title">
+          {header[0]}
+          <span className="keyword">{header[1]}</span>
+        </h2>
+        <ul
+          className="unorder"
+          style={{ listStyleImage: 'url(/icon/check.svg)' }}
+        >
+          <li className="list">{list[0]}</li>
+          <li className="list">{list[1]}</li>
+          <li className="list">{list[2]}</li>
+        </ul>
+        <footer className="action">
+          <Link href="/dashboard">
+            <a className="enroll">
+              {action}
+              <img
+                className="icon"
+                src="/icon/chevron_right.svg"
+                alt="Chevron"
+              />
+            </a>
+          </Link>
+          <Link href="/tasks">
+            <a className="explore">{subAction}</a>
+          </Link>
+        </footer>
+        {/* <a className="provider">Get start now, it's free.</a> */}
+      </article>
+      <figure className="cover">
+        <picture className="container">
+          <img
+            className="image"
+            src="https://vignette.wikia.nocookie.net/himoto-umaruchan/images/a/a2/Umaru%27s_anime_design_%28chibi%29.png/revision/latest?cb=20200411195915"
+          />
+        </picture>
+      </figure>
+    </section>
+  )
+}
 
 export default Introduction
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -12,130 +12,137 @@ import {
   Featured
 } from '../components/landing/featured'
 
-export default () => (
-  <PageLayout>
-    <Introduction />
-    <Row>
-      <Detail title="Integrated Environment">
-        Go beyond code, syntax-highlighting, close an unused block, smart
-        completions for the best exclusive experience.
-      </Detail>
-      <Detail title="Crafted by Specialists">
-        Over 400 problems designed and curated by specialists. To deliver the
-        most comprehensive learning experience possible.
-      </Detail>
-      <Detail title="Trusted by thousands">
-        Join other 10,000 to craft, learn and practice, on one of the best
-        data-structure and algorithms solving platform.
-      </Detail>
-    </Row>
+import intl from '../intl/index.json'
 
-    {/* Featured Contest */}
-    {/**
-     * ! The logic here should be real-time, sync with Firebase.
-     * ? Shouldn't be displayed when there's no contest.
-     *  */}
-    <Featured>Featured Contest</Featured>
-    <FeaturedRow>
-      <Card
-        title="Google Code Jam"
-        href="/"
-        src="https://cdn-0.tierlistmania.com/wp-content/uploads/2019/07/Best-Ships-Tier-List-Azur-Lane.png"
-        alt="アズールレーン"
+const Index = () => {
+  // Transform this to state management engine.
+  const lang = 'en'
+
+  const translated = intl[lang]
+
+  const contestData = [
+    {
+      href: '/',
+      src:
+        'https://cdn-0.tierlistmania.com/wp-content/uploads/2019/07/Best-Ships-Tier-List-Azur-Lane.png',
+      alt: 'アズールレーン'
+    },
+    {
+      href: '/',
+      src: 'https://pbs.twimg.com/media/DundVDVUUAA89c8.jpg',
+      alt: 'Houkai Impact 3rd'
+    },
+    {
+      href: '/',
+      src:
+        'http://i0.wp.com/smartgamecap.net/wp-content/uploads/2020/02/photo.jpg?fit=1050%2C590%2C590&ssl=1',
+      alt: 'アークナイツ'
+    }
+  ]
+
+  const problemData = [
+    {
+      href: '/'
+    },
+    {
+      href: '/'
+    },
+    {
+      href: '/'
+    },
+    {
+      href: 'https://www.npmjs.com/package/instant-sort'
+    }
+  ]
+
+  // If 'isReverse' is set to true, justifyContent is aliged as flex-end.
+  const showcaseAssets = [
+    {
+      src: '/assets/img/dashboard.png',
+      alt: 'Dashboard Demo',
+      isReverse: false
+    },
+    {
+      src: '/assets/img/ide.png',
+      alt: 'IDE Showcase',
+      isReverse: true
+    },
+    {
+      src: '/assets/img/dashboard.png',
+      alt: 'IDK what to put here yet, so...',
+      isReverse: false
+    }
+  ]
+
+  return (
+    <PageLayout>
+      <Introduction />
+      <Row>
+        {translated.feature.map(({ title, detail }) => (
+          <Detail title={title}>{detail}</Detail>
+        ))}
+      </Row>
+
+      {/* Featured Contest */}
+      {/**
+       * ! The logic here should be real-time, sync with Firebase.
+       * ? Shouldn't be displayed when there's no contest.
+       *  */}
+      <Featured>{translated.contest.title}</Featured>
+      <FeaturedRow>
+        {translated.contest.list.map(({ title, detail }, index) => (
+          <Card
+            title={title}
+            href={contestData[index].href}
+            src={contestData[index].src}
+            alt={contestData[index].alt}
+            key={title}
+          >
+            {detail}
+          </Card>
+        ))}
+      </FeaturedRow>
+
+      {/* Latest Problems */}
+      <Featured>{translated.problems.title}</Featured>
+      <FeaturedRow>
+        {translated.problems.list.map(({ title, detail }, index) => (
+          <Card title={title} href={problemData[index].href} key={title}>
+            {detail}
+          </Card>
+        ))}
+      </FeaturedRow>
+
+      <Divider />
+
+      {/* Showcase */}
+      {translated.showcase.map(({ title, detail }, index) => (
+        <Showcase
+          title={title}
+          src={showcaseAssets[index].src}
+          alt={showcaseAssets[index].src}
+          key={title}
+          reverse={showcaseAssets[index].isReverse}
+        >
+          {detail}
+        </Showcase>
+      ))}
+
+      {/* Last Chance for asking to join the platform */}
+      <Join />
+      {/* Some cool thing placeholder */}
+      <div
+        style={{
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          height: '500px'
+        }}
       >
-        Put your coding skills to the test as you work your way through multiple
-        rounds of algorithmic coding puzzles for the title of Code Jam Champ and
-        15000 USD.
-      </Card>
-      <Card
-        title="Microsoft Imagine Cup"
-        href="/"
-        src="https://pbs.twimg.com/media/DundVDVUUAA89c8.jpg"
-        alt="Houkai Impact 3rd"
-      >
-        Global competition that empowers the next generation of computer science
-        students to team up and use their creativity, passion and knowledge of
-        technology.
-      </Card>
-      <Card
-        title="Google Code In"
-        href="/"
-        src="https://i0.wp.com/smartgamecap.net/wp-content/uploads/2020/02/photo.jpg?fit=1050%2C590&ssl=1"
-        alt="アークナイツ"
-      >
-        Google's contest introducing 13-17 year old pre-university students to
-        open source software development.
-      </Card>
-    </FeaturedRow>
+        Put some cool artwork here
+      </div>
+    </PageLayout>
+  )
+}
 
-    {/* Latest Problems */}
-    <Featured>New Problems</Featured>
-    <FeaturedRow>
-      <Card title="Tortoise and Hare" href="/">
-        Floyd's "tortoise and hare" cycle detection algorithm, applied to the
-        sequence 2, 0, 6, 3, 1, 6, 3, 1
-      </Card>
-      <Card title="Permutation Sort" href="/">
-        Randomly Shuffle an entire array until it completely sorted.
-      </Card>
-      <Card title="Red Black Tree" href="/">
-        Every node is either red or black. Every leaf (NULL) is black. If a node
-        is red, then both its children are black.
-      </Card>
-      <Card
-        title="Instant Sort"
-        href="https://www.npmjs.com/package/instant-sort"
-      >
-        The fastest sorting algorithm ever with the speed of O(1).
-      </Card>
-    </FeaturedRow>
-
-    <Divider />
-
-    {/* Showcase */}
-    <Showcase
-      title="Exclusive Data-Science Level Analytic"
-      src="/assets/img/dashboard.png"
-      alt="Dashboard Demo"
-    >
-      To enhance the best experience, tracking and analyzing is proven to be the
-      best for planning future skills, and growth learning. Especially at
-      programming.in.th, we crafted the finest dashboard for everyone.
-    </Showcase>
-    <Showcase
-      title="Integrated Development Environment"
-      src="/assets/img/ide.png"
-      alt="Dashboard Demo"
-      reverse
-    >
-      Beyond the thought, environment of programming.in.th is perfectly crafted
-      for integrated development environment which result a perfect workspace
-      for solving and experimenting an algorithm without a help of a code
-      editor.
-    </Showcase>
-    <Showcase
-      title="Exclusive Data-Science Level Analytic"
-      src="/assets/img/dashboard.png"
-      alt="IDK what to put here yet, so..."
-    >
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-      tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-      veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-      commodo consequat.
-    </Showcase>
-
-    {/* Last Chance for asking to join the platform */}
-    <Join />
-    {/* Some cool thing placeholder */}
-    <div
-      style={{
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        height: '500px'
-      }}
-    >
-      Put some cool Thing here
-    </div>
-  </PageLayout>
-)
+export default Index
