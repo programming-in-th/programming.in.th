36d29f93323837e404f9d08310f35b1d048ff802
win11905 <donothavemoney@gmail.com>
2020-06-01 02:36:59 +0700

    🔥Remove unused code

--- src/pages/submissions/[id].tsx
+++ src/pages/submissions/[id].tsx
@@ -73,7 +73,6 @@ const SubmissionDetail: NextPage = () => {
       unsubscribe()
     }
   }, [])
-  useEffect(() => {}, [submission])
 
   const [currentCodeIndex, setCurrentCodeIndex] = useState<number>(0)
 
