486aacdac42a7615455c1fe8a4451aaa4f58c872
Possawat Suksai <markkung08@gmail.com>
2020-06-21 17:49:57 +0700

    :sparkles: Task list (#439)

--- src/pages/tasks/index.tsx
+++ src/pages/tasks/index.tsx
@@ -1,14 +1,53 @@
 import React from 'react'
-import { Text, Flex } from '@chakra-ui/core'
+import { GetStaticProps } from 'next'
+import { Flex } from '@chakra-ui/core'
 
 import { PageLayout } from 'components/Layout'
+import db from 'lib/firebase-admin'
+import { TaskTable } from 'components/tasks/Table'
 
-export default () => (
-  <PageLayout>
-    <Flex align="center" justify="center" flexGrow={1}>
-      <Text fontSize="6xl" fontWeight="800" textAlign="center">
-        TASKS
-      </Text>
-    </Flex>
-  </PageLayout>
-)
+export default ({ result }) => {
+  const columns = React.useMemo(
+    () => [
+      {
+        Header: 'PROBLEM ID',
+        accessor: 'id',
+      },
+      {
+        Header: 'PROBLEM TITLE',
+        accessor: 'title',
+      },
+    ],
+    []
+  )
+
+  return (
+    <PageLayout>
+      <Flex justify="center" flexGrow={1} py={4} px={4}>
+        <TaskTable result={result} columns={columns} />
+      </Flex>
+    </PageLayout>
+  )
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+  const taskDocs = await db()
+    .collection('tasks')
+    .where('visible', '==', true)
+    .get()
+
+  const result = []
+
+  for (const doc of taskDocs.docs) {
+    const data = doc.data()
+    data.id = doc.id
+    result.push(data)
+  }
+
+  return {
+    props: {
+      result,
+    },
+    unstable_revalidate: 60,
+  }
+}
