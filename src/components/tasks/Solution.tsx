539dfcaae64e92aa12577f4839ef76b965817f7c
PeppaPigHS <ppeppapig.stuff@gmail.com>
2020-06-30 14:13:46 +0700

    :bug: Remove Head from Solution

--- src/components/tasks/Solution.tsx
+++ src/components/tasks/Solution.tsx
@@ -15,14 +15,6 @@ export const Solution = ({ solution }) => {
 
   return (
     <React.Fragment>
-      <Head>
-        <link
-          rel="stylesheet"
-          href="https://cdn.jsdelivr.net/npm/katex@0.11.1/dist/katex.min.css"
-          integrity="sha384-zB1R0rpPzHqg7Kpt0Aljp8JPLqbXI3bhnPWROx27a9N0Ll6ZP/+DiW/UqRcLbRjq"
-          crossOrigin="anonymous"
-        />
-      </Head>
       <Box p={[6, 0]} mt={[0, 4]} mx={[0, 'auto']} w={['100%', 800]}>
         <MarkDownStyle>
           <div dangerouslySetInnerHTML={{ __html: solution }}></div>
