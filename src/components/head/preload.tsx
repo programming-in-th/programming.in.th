ea0e03ccfe807e893c19ff3a394278d657a647cb
PeppaPigHS <ppeppapig.stuff@gmail.com>
2020-06-30 15:19:39 +0700

    :bug: Fix latex not rendering

--- src/components/head/preload.tsx
+++ src/components/head/preload.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 export const Preload = () => (
   <React.Fragment>
     <link
-      rel="preload"
+      rel="stylesheet"
       href="https://cdn.jsdelivr.net/npm/katex@0.11.1/dist/katex.min.css"
       integrity="sha384-zB1R0rpPzHqg7Kpt0Aljp8JPLqbXI3bhnPWROx27a9N0Ll6ZP/+DiW/UqRcLbRjq"
       crossOrigin="anonymous"
