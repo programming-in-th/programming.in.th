2ea747b3243c5ab8ee5db2829f4c1e9739b89cae
Pongsaphol Pongsawakul <30414878+win11905@users.noreply.github.com>
2020-07-02 00:45:21 +0700

    🐛 Fixed date format

--- src/utils/getTimestamp.ts
+++ src/utils/getTimestamp.ts
@@ -2,7 +2,7 @@ export const getTimestamp = (timestamp: any) => {
   const second = timestamp.seconds ? timestamp.seconds : timestamp._seconds
   const time = new Date(second * 1000)
   const dd = (time.getDate() < 10 ? '0' : '') + time.getDate()
-  const mm = (time.getMonth() < 10 ? 1 : 0) + time.getMonth()
+  const mm = (time.getMonth() < 10 ? '0' : '') + (time.getMonth() + 1)
   const yyyy = time.getFullYear() + 543
   const tt = time.toString().substring(16, 24)
   const humanTimestamp = `${dd}/${mm}/${yyyy} ${tt}`
