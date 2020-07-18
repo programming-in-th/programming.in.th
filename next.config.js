bed043abb21686b245262183097f0f28908ac267
SaltyAom <aomkirby@mystiar.com>
2020-07-18 16:58:30 +0700

    :wrench: Fix Safari wrong size image

--- next.config.js
+++ next.config.js
@@ -1,7 +1,6 @@
 const withPlugins = require('next-compose-plugins')
 
 const withOffline = require('next-offline')
-const withBundleAnalyzer = require('@zeit/next-bundle-analyzer')
 
 const remarkMath = require('remark-math')
 const rehypeKatex = require('rehype-katex')
@@ -10,12 +9,16 @@ const rehypePrism = require('@mapbox/rehype-prism')
 const withCSS = require('@zeit/next-css')
 const withStylus = require('@zeit/next-stylus')
 
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+})
+
 const withMDX = require('@zeit/next-mdx')({
   extension: /\.mdx?$/,
   options: {
     remarkPlugins: [remarkMath],
-    rehypePlugins: [rehypeKatex, rehypePrism]
-  }
+    rehypePlugins: [rehypeKatex, rehypePrism],
+  },
 })
 
 module.exports = withPlugins(
@@ -23,31 +26,20 @@ module.exports = withPlugins(
     [
       withMDX,
       {
-        pageExtensions: ['tsx', 'mdx']
-      }
+        pageExtensions: ['tsx', 'mdx'],
+      },
     ],
     withBundleAnalyzer,
     withOffline,
     withCSS,
-    withStylus
+    withStylus,
   ],
   {
-    analyzeServer: ['server', 'both'].includes(process.env.BUNDLE_ANALYZE),
-    analyzeBrowser: ['browser', 'both'].includes(process.env.BUNDLE_ANALYZE),
-    bundleAnalyzerConfig: {
-      server: {
-        analyzerMode: 'static',
-        reportFilename: '../bundles/server.html'
-      },
-      browser: {
-        analyzerMode: 'static',
-        reportFilename: '../bundles/client.html'
-      }
-    },
-    target: 'serverless',
-    transformManifest: manifest => ['/'].concat(manifest),
+    transformManifest: (manifest) => ['/'].concat(manifest),
     workboxOpts: {
-      swDest: 'static/service-worker.js',
+      swDest: process.env.NEXT_EXPORT
+        ? 'service-worker.js'
+        : 'static/service-worker.js',
       runtimeCaching: [
         {
           urlPattern: /^https?.*/,
@@ -57,12 +49,12 @@ module.exports = withPlugins(
             networkTimeoutSeconds: 15,
             expiration: {
               maxEntries: 150,
-              maxAgeSeconds: 30 * 24 * 60 * 60 // 1 month
+              maxAgeSeconds: 30 * 24 * 60 * 60, // 1 month
             },
             cacheableResponse: {
-              statuses: [0, 200]
-            }
-          }
+              statuses: [0, 200],
+            },
+          },
         },
         {
           urlPattern: /\.(?:png|jpg|jpeg|svg)$/,
@@ -72,12 +64,12 @@ module.exports = withPlugins(
             cacheName: 'images',
             expiration: {
               maxEntries: 150,
-              maxAgeSeconds: 30 * 24 * 60 * 60 // 1 month
+              maxAgeSeconds: 30 * 24 * 60 * 60, // 1 month
             },
             cacheableResponse: {
-              statuses: [0, 200]
-            }
-          }
+              statuses: [0, 200],
+            },
+          },
         },
         {
           urlPattern: /^https:\/\/fonts\.gstatic\.com/,
@@ -86,14 +78,24 @@ module.exports = withPlugins(
           options: {
             cacheName: 'google-fonts',
             expiration: {
-              maxAgeSeconds: 60 * 60 * 24 * 365 // 1 years
+              maxAgeSeconds: 60 * 60 * 24 * 365, // 1 years
             },
             cacheableResponse: {
-              statuses: [0, 200]
-            }
-          }
-        }
-      ]
-    }
+              statuses: [0, 200],
+            },
+          },
+        },
+      ],
+    },
+    experimental: {
+      async rewrites() {
+        return [
+          {
+            source: '/service-worker.js',
+            destination: '/_next/static/service-worker.js',
+          },
+        ]
+      },
+    },
   }
 )
