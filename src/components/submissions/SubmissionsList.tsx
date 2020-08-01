059145a056020b2c219a6651ddb377c73b2a4982
win11905 <donothavemoney@gmail.com>
2020-08-01 22:55:07 +0700

    ✨ move to swr 3.0

--- src/components/submissions/SubmissionsList.tsx
+++ src/components/submissions/SubmissionsList.tsx
@@ -36,7 +36,6 @@ export const SubmissionsList = ({ id: taskFrom }) => {
 
   const Submissions = () => {
     const pageSize = 10
-    const preload = 1
     const { data, error, size, setSize } = useSWRInfinite(
       (index, previousData) => {
         return `${config.baseURL}/getSubmissions?limit=${pageSize}&next=${
@@ -46,33 +45,12 @@ export const SubmissionsList = ({ id: taskFrom }) => {
       SWRfetch
     )
 
-    const [key, setKey] = useState<string>('')
-    const [ipage, setIpage] = useState<number>(0)
-
-    useEffect(() => {
-      if (data) {
-        const _key = `pagination@${username}-${task}`
-        if (key !== _key) {
-          setKey(_key)
-          if (!cache.has(_key)) {
-            cache.set(_key, 1)
-          }
-          const pageLength = cache.get(_key)
-          if (size !== pageLength + preload) {
-            setSize((size) => pageLength + preload)
-          }
-          setIpage(pageLength)
-        }
-      }
-    }, [data])
-
     const isLoadingInitialData = !data && !error
     const isLoadingMore =
-      isLoadingInitialData || (data && typeof data[ipage - 1] === 'undefined')
+      isLoadingInitialData || (data && typeof data[size - 1] === 'undefined')
     const isEmpty = data?.[0]?.length === 0
     const isReachingEnd =
       isEmpty || (data && data[data.length - 1]?.next === null)
-    const isReachingEndF = isEmpty || (data && data[ipage - 1]?.next === null)
 
     return (
       <React.Fragment>
@@ -100,62 +78,57 @@ export const SubmissionsList = ({ id: taskFrom }) => {
 
             <tbody>
               {data &&
-                data
-                  .slice(0, ipage)
-                  .map(
-                    (submissions: {
-                      data: ISubmissionList[]
-                      next: string
-                    }) => {
-                      return submissions.data.map(
-                        (submission: ISubmissionList) => {
-                          const TdLink = ({ children }) => {
-                            return (
-                              <Td>
-                                <Link
-                                  href="/submissions/[id]"
-                                  as={`/submissions/${submission.submissionID}`}
-                                >
-                                  <a>
-                                    <Box h="100%" w="100%" padding="8px 16px">
-                                      {children}
-                                    </Box>
-                                  </a>
-                                </Link>
-                              </Td>
-                            )
-                          }
+                data.map(
+                  (submissions: { data: ISubmissionList[]; next: string }) => {
+                    return submissions.data.map(
+                      (submission: ISubmissionList) => {
+                        const TdLink = ({ children }) => {
                           return (
-                            <React.Fragment key={submission.submissionID}>
-                              {isObjectEmpty(submission) ? (
-                                <Tr>
-                                  <TdHide colSpan={7} />
-                                </Tr>
-                              ) : (
-                                <Tr>
-                                  <TdLink>
-                                    {getTimestamp(submission.timestamp)}
-                                  </TdLink>
-                                  <TdLink>{submission.username}</TdLink>
-                                  <TdLink>{submission.taskID}</TdLink>
-                                  <TdLink>{submission.score}</TdLink>
-                                  <TdLink>
-                                    {
-                                      arrToObj(config.languageData)[
-                                        submission.language
-                                      ]
-                                    }
-                                  </TdLink>
-                                  <TdLink>{submission.time}</TdLink>
-                                  <TdLink>{submission.memory}</TdLink>
-                                </Tr>
-                              )}
-                            </React.Fragment>
+                            <Td>
+                              <Link
+                                href="/submissions/[id]"
+                                as={`/submissions/${submission.submissionID}`}
+                              >
+                                <a>
+                                  <Box h="100%" w="100%" padding="8px 16px">
+                                    {children}
+                                  </Box>
+                                </a>
+                              </Link>
+                            </Td>
                           )
                         }
-                      )
-                    }
-                  )}
+                        return (
+                          <React.Fragment key={submission.submissionID}>
+                            {isObjectEmpty(submission) ? (
+                              <Tr>
+                                <TdHide colSpan={7} />
+                              </Tr>
+                            ) : (
+                              <Tr>
+                                <TdLink>
+                                  {getTimestamp(submission.timestamp)}
+                                </TdLink>
+                                <TdLink>{submission.username}</TdLink>
+                                <TdLink>{submission.taskID}</TdLink>
+                                <TdLink>{submission.score}</TdLink>
+                                <TdLink>
+                                  {
+                                    arrToObj(config.languageData)[
+                                      submission.language
+                                    ]
+                                  }
+                                </TdLink>
+                                <TdLink>{submission.time}</TdLink>
+                                <TdLink>{submission.memory}</TdLink>
+                              </Tr>
+                            )}
+                          </React.Fragment>
+                        )
+                      }
+                    )
+                  }
+                )}
             </tbody>
           </Table>
         </Box>
@@ -164,17 +137,13 @@ export const SubmissionsList = ({ id: taskFrom }) => {
             if (!isReachingEnd) {
               setSize((size) => size + 1)
             }
-            if (!isReachingEndF) {
-              cache.set(key, cache.get(key) + 1)
-              setIpage(cache.get(key))
-            }
           }}
           isLoading={isLoadingMore}
-          isDisabled={isReachingEndF || isLoadingMore}
+          isDisabled={isLoadingMore || isReachingEnd}
           mt={4}
           width="100%"
         >
-          {isReachingEndF ? 'No more submission' : 'Load more'}
+          {isReachingEnd ? 'No more submission' : 'Load more'}
         </Button>
       </React.Fragment>
     )
