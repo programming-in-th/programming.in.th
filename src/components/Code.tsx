08bf6499ec25f5f21fb0c6dcb5b2cba224955008
Possawat Suksai <markkung08@gmail.com>
2020-08-03 00:34:17 +0700

    :sparkles: Add new language support (#545)
    * 🐛Fix compatibility of submissionList in task page (#544)
    
    * :arrow_up: Bump elliptic from 6.5.2 to 6.5.3 (#538)
    
    Bumps [elliptic](https://github.com/indutny/elliptic) from 6.5.2 to 6.5.3.
    - [Release notes](https://github.com/indutny/elliptic/releases)
    - [Commits](https://github.com/indutny/elliptic/compare/v6.5.2...v6.5.3)
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    Co-authored-by: Possawat Suksai <markkung08@gmail.com>
    
    * ✨ Support various language (#539)
    
    Co-authored-by: Pongsaphol Pongsawakul <30414878+win11905@users.noreply.github.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>

--- src/components/Code.tsx
+++ src/components/Code.tsx
@@ -2,10 +2,13 @@ import React from 'react'
 import Highlight, { Prism } from 'prism-react-renderer'
 
 import { Flex, Box } from '@chakra-ui/core'
+;((typeof global !== 'undefined' ? global : window) as any).Prism = Prism
+
+require('prismjs/components/prism-rust')
 
 export interface CodeBlockProps {
   code: string
-  language: 'c' | 'cpp' | 'python'
+  language: string
 }
 
 export const Code = ({ code, language }: CodeBlockProps) => {
