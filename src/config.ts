1d7c98526287385cac551dd68b0c08a54356e9b7
Pongsaphol Pongsawakul <30414878+win11905@users.noreply.github.com>
2020-08-07 13:06:20 +0700

    ⚡️ gather aws URL to config (#562)
    * ⚡️ gather aws URL config
    
    * 🔥 Remove null check
    
    Co-authored-by: Possawat Suksai <markkung08@gmail.com>

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,6 @@
 export const config = {
   baseURL: 'https://asia-east2-proginth.cloudfunctions.net',
+  awsURL: 'https://beta-programming-in-th.s3-ap-southeast-1.amazonaws.com',
   languageData: [
     ['cpp14', 'C++', 'cpp'],
     ['c11', 'C', 'c'],
