4c019a52b1d9d67a51da778eab1de0c3ce6b2e64
PeppaPigHS <ppeppapig.stuff@gmail.com>
2020-08-07 16:50:53 +0700

    :sparkles: Add copy code button

--- src/components/Code.tsx
+++ src/components/Code.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Highlight, { Prism } from 'prism-react-renderer'
 
-import { Flex, Box } from '@chakra-ui/core'
+import { Flex, Box, Button, useClipboard } from '@chakra-ui/core'
 ;((typeof global !== 'undefined' ? global : window) as any).Prism = Prism
 
 require('prismjs/components/prism-rust')
@@ -12,45 +12,62 @@ export interface CodeBlockProps {
 }
 
 export const Code = ({ code, language }: CodeBlockProps) => {
+  const { onCopy, hasCopied } = useClipboard(code)
+
   return (
-    <Highlight Prism={Prism} code={code} language={language as any}>
-      {({ style, tokens, getLineProps, getTokenProps }) => (
-        <Flex
-          as="pre"
-          overflowY="auto"
-          style={{ ...style }}
-          fontSize="13.6px !important"
-          className={`language-${language}`}
-        >
-          <code className={`language-${language}`}>
-            {tokens.map((line, i) => (
-              <Box
-                as="tr"
-                key={i}
-                {...getLineProps({ line, key: i })}
-                display="table-row"
-              >
+    <Box position="relative" zIndex={0}>
+      <Highlight Prism={Prism} code={code} language={language as any}>
+        {({ style, tokens, getLineProps, getTokenProps }) => (
+          <Flex
+            as="pre"
+            overflowY="auto"
+            style={{ ...style }}
+            fontSize="13.6px !important"
+            className={`language-${language}`}
+            w="100%"
+          >
+            <code className={`language-${language}`}>
+              {tokens.map((line, i) => (
                 <Box
-                  as="td"
-                  key={i + 'l'}
-                  color="gray.400"
-                  pr={2}
-                  width={8}
-                  display="table-cell"
-                  userSelect="none"
+                  as="tr"
+                  key={i}
+                  {...getLineProps({ line, key: i })}
+                  display="table-row"
                 >
-                  {i + 1}
+                  <Box
+                    as="td"
+                    key={i + 'l'}
+                    color="gray.400"
+                    pr={2}
+                    width={8}
+                    display="table-cell"
+                    userSelect="none"
+                  >
+                    {i + 1}
+                  </Box>
+                  <Box display="table-cell" as="td">
+                    {line.map((token, key) => (
+                      <span key={key} {...getTokenProps({ token, key })} />
+                    ))}
+                  </Box>
                 </Box>
-                <Box display="table-cell" as="td">
-                  {line.map((token, key) => (
-                    <span key={key} {...getTokenProps({ token, key })} />
-                  ))}
-                </Box>
-              </Box>
-            ))}
-          </code>
-        </Flex>
-      )}
-    </Highlight>
+              ))}
+            </code>
+          </Flex>
+        )}
+      </Highlight>
+      <Button
+        size="sm"
+        position="absolute"
+        textTransform="uppercase"
+        fontSize="xs"
+        height="24px"
+        top={4}
+        right="1.25em"
+        onClick={onCopy}
+      >
+        {hasCopied ? 'copied' : 'copy'}
+      </Button>
+    </Box>
   )
 }
