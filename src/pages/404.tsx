d0a99fce73b8d97026afb509ad6d37d74de85960
win11905 <donothavemoney@gmail.com>
2021-08-12 23:16:59 -0400

    Add Index & 404

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,16 +1,63 @@
 import React from 'react'
-import { Text, Flex } from '@chakra-ui/core'
 
-import { PageLayout } from 'components/Layout'
+import Link from 'next/link'
 
 const FourZeroFour = () => (
-  <PageLayout>
-    <Flex align="center" justify="center" flexGrow={1}>
-      <Text fontSize="6xl" fontWeight="800" textAlign="center">
-        404 | Not Found
-      </Text>
-    </Flex>
-  </PageLayout>
+  <div className="min-h-screen pt-16 pb-12 flex flex-col bg-white">
+    <main className="flex-grow flex flex-col justify-center max-w-7xl w-full mx-auto px-4 sm:px-6 lg:px-8">
+      <div className="flex-shrink-0 flex justify-center">
+        <a href="/" className="inline-flex">
+          <span className="sr-only">Workflow</span>
+          <img
+            className="h-12 w-auto"
+            src="https://tailwindui.com/img/logos/workflow-mark.svg?color=gray&shade=600"
+            alt=""
+          />
+        </a>
+      </div>
+      <div className="py-16">
+        <div className="text-center">
+          <p className="text-sm font-semibold text-gray-600 uppercase tracking-wide">
+            404 error
+          </p>
+          <h1 className="mt-2 text-4xl font-extrabold text-gray-900 tracking-tight sm:text-5xl">
+            Page not found.
+          </h1>
+          <p className="mt-2 text-base text-gray-500">
+            Sorry, we couldn’t find the page you’re looking for.
+          </p>
+          <div className="mt-6">
+            <Link href="/">
+              <a className="text-base font-medium text-gray-600 hover:text-gray-500">
+                Go back home<span aria-hidden="true"> &rarr;</span>
+              </a>
+            </Link>
+          </div>
+        </div>
+      </div>
+    </main>
+    <footer className="flex-shrink-0 max-w-7xl w-full mx-auto px-4 sm:px-6 lg:px-8">
+      <nav className="flex justify-center space-x-4">
+        <a
+          href="#"
+          className="text-sm font-medium text-gray-500 hover:text-gray-600"
+        >
+          Contact Support
+        </a>
+        <span
+          className="inline-block border-l border-gray-300"
+          aria-hidden="true"
+        />
+        <a
+          target="_blank"
+          href="https://www.facebook.com/programming.in.th"
+          className="text-sm font-medium text-gray-500 hover:text-gray-600"
+        >
+          Facebook
+        </a>
+      </nav>
+    </footer>
+  </div>
 )
 
 export default FourZeroFour
---
+++ src/pages/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import { PageLayout } from 'components/Layout'
+
+const Index = () => (
+  <PageLayout>
+    <div className="flex items-center justify-center h-screen">
+      <p className="text-6xl">hello</p>
+    </div>
+  </PageLayout>
+)
+
+export default Index
