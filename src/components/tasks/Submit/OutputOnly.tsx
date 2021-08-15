02d4e75abc8d2b3e283cef4f572f58176c1be2e3
win11905 <donothavemoney@gmail.com>
2021-08-15 19:19:39 -0400

    fixed deepscan

--- src/components/tasks/Submit/OutputOnly.tsx
+++ src/components/tasks/Submit/OutputOnly.tsx
@@ -1,14 +1,10 @@
 import React from 'react'
 
-import { useUser } from '../../UserContext'
-
 import { UploadCode } from '../../Upload'
 import { useStatus } from './useStatus'
 import { useSubmit } from './useSubmit'
 
 export const OutputOnly = ({ metadata }) => {
-  const { user } = useUser()
-
   const {
     submit,
     codeFile,
