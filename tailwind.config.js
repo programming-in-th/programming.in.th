1229ce82a640cf3151c2be17ef24243123977d86
Possawat Suksai <markkung08@gmail.com>
2021-08-21 00:52:38 +0700

    Change Kanit to IBM Plex Thai

--- tailwind.config.js
+++ tailwind.config.js
@@ -85,7 +85,7 @@ module.exports = {
         'Inter var',
         'SF Pro Display',
         'Sukhumvit Set',
-        'Kanit',
+        'IBM Plex Sans Thai',
         ...defaultTheme.fontFamily.sans,
       ],
     },
@@ -117,11 +117,81 @@ module.exports = {
         },
         {
           '@font-face': {
-            fontFamily: 'Kanit',
+            fontFamily: 'IBM Plex Sans Thai',
             fontStyle: 'normal',
             fontWeight: '400',
             fontDisplay: 'swap',
-            src: 'local("Kanit Regular"), local("Kanit-Regular"), url(https://fonts.gstatic.com/s/kanit/v7/nKKZ-Go6G5tXcraBGwCKd6xBDFs.woff2) format("woff2")',
+            src: 'local("IBM Plex Sans Thai"), local("IBMPlexSansThai"), url("/fonts/IBMPlexSansThai-Regular.woff2") format("woff2")',
+          },
+        },
+        {
+          '@font-face': {
+            fontFamily: 'IBM Plex Sans Thai',
+            fontStyle: 'normal',
+            fontWeight: '450',
+            fontDisplay: 'swap',
+            src: 'local("IBM Plex Sans Thai Text"), local("IBMPlexSansThai-Text"), url("/fonts/IBMPlexSansThai-Text.woff2") format("woff2")',
+          },
+        },
+        {
+          '@font-face': {
+            fontFamily: 'IBM Plex Sans Thai',
+            fontStyle: 'normal',
+            fontWeight: '500',
+            fontDisplay: 'swap',
+            src: 'local("IBM Plex Sans Thai Medium"), local("IBMPlexSansThai-Medium"), url("/fonts/IBMPlexSansThai-Medium.woff2") format("woff2")',
+          },
+        },
+        {
+          '@font-face': {
+            fontFamily: 'IBM Plex Sans Thai',
+            fontStyle: 'normal',
+            fontWeight: '600',
+            fontDisplay: 'swap',
+            src: 'local("IBM Plex Sans Thai SemiBold"), local("IBMPlexSansThai-SemiBold"), url("/fonts/IBMPlexSansThai-SemiBold.woff2") format("woff2")',
+          },
+        },
+        {
+          '@font-face': {
+            fontFamily: 'IBM Plex Sans Thai',
+            fontStyle: 'normal',
+            fontWeight: '700',
+            fontDisplay: 'swap',
+            src: 'local("IBM Plex Sans Thai Bold"), local("IBMPlexSansThai-Bold"), url("/fonts/IBMPlexSansThai-Bold.woff2") format("woff2")',
+          },
+        },
+        {
+          '@font-face': {
+            fontFamily: 'IBM Plex Sans Thai',
+            fontStyle: 'normal',
+            fontWeight: '100',
+            fontDisplay: 'swap',
+            src: 'local("IBM Plex Sans Thai Thin"), local("IBMPlexSansThai-Thin"), url("/fonts/IBMPlexSansThai-Thin.woff2") format("woff2")',
+          },
+        },
+        {
+          '@font-face': {
+            fontFamily: 'IBM Plex Sans Thai',
+            fontStyle: 'normal',
+            fontWeight: '200',
+            fontDisplay: 'swap',
+            src: 'local("IBM Plex Sans Thai ExtraLight"), local("IBMPlexSansThai-ExtraLight"), url("/fonts/IBMPlexSansThai-ExtraLight.woff2") format("woff2")',
+          },
+        },
+        {
+          '@font-face': {
+            fontFamily: 'IBM Plex Sans Thai',
+            fontStyle: 'normal',
+            fontWeight: '300',
+            fontDisplay: 'swap',
+            src: 'local("IBM Plex Sans Thai Light"), local("IBMPlexSansThai-Light"), url("/fonts/IBMPlexSansThai-Light.woff2") format("woff2")',
+          },
+        },
+        {
+          '@font-face': {
+            fontFamily: 'Sukhumvit Set',
+            fontDisplay: 'swap',
+            src: "local('Sukhumvit Set')",
             unicodeRange: 'U+0E01-0E5B, U+200C-200D, U+25CC',
           },
         },
