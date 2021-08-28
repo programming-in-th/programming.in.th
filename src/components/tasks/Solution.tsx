0307bb7ecda2be9a47a26fb45887f0808b289425
Possawat Suksai <markkung08@gmail.com>
2021-08-28 23:41:16 +0700

    Fix markdown rendering

--- src/components/tasks/Solution.tsx
+++ src/components/tasks/Solution.tsx
@@ -5,7 +5,7 @@ export const Solution = ({ solution }) => {
   if (!solution) {
     return (
       <div className="flex items-center justify-center flex-grow">
-        <p className="text-6xl font-extrabold text-center p-8">
+        <p className="p-8 text-6xl font-extrabold text-center">
           Solution does not exist!
         </p>
       </div>
@@ -17,12 +17,12 @@ export const Solution = ({ solution }) => {
       <Head>
         <link
           rel="stylesheet"
-          href="https://cdn.jsdelivr.net/npm/katex@0.11.1/dist/katex.min.css"
-          integrity="sha384-zB1R0rpPzHqg7Kpt0Aljp8JPLqbXI3bhnPWROx27a9N0Ll6ZP/+DiW/UqRcLbRjq"
+          href="https://cdn.jsdelivr.net/npm/katex@0.13.13/dist/katex.min.css"
+          integrity="sha384-RZU/ijkSsFbcmivfdRBQDtwuwVqK7GMOw6IMvKyeWL2K5UAlyp6WonmB8m7Jd0Hn"
           crossOrigin="anonymous"
         />
       </Head>
-      <div className="p-6 md:p-0 md:mt-4 md:mx-auto w-full max-w-4xl">
+      <div className="w-full max-w-4xl p-6 md:p-0 md:mt-4 md:mx-auto">
         <div
           className="prose lg:prose-lg xl:prose-xl"
           dangerouslySetInnerHTML={{ __html: solution }}
--- src/lib/renderMarkdown.ts
+++ src/lib/renderMarkdown.ts
@@ -1,17 +1,10 @@
-import unified from 'unified'
+import { unified } from 'unified'
 import parse from 'remark-parse'
 import remark2rehype from 'remark-rehype'
 import math from 'remark-math'
 import katex from 'rehype-katex'
 import highlight from '@mapbox/rehype-prism'
 import html from 'rehype-stringify'
-import sanitize from 'rehype-sanitize'
-
-import githubSchema from 'hast-util-sanitize/lib/github.json'
-
-githubSchema.attributes['*'].push('className')
-
-// import sanitizeSetting from './sanitizer.json'
 
 export const renderMarkdown = async (content: string) => {
   try {
@@ -19,7 +12,6 @@ export const renderMarkdown = async (content: string) => {
       .use(parse)
       .use(math)
       .use(remark2rehype)
-      .use(sanitize, githubSchema)
       .use(katex)
       .use(highlight)
       .use(html)
