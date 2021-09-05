1545c4aa0a2e92cccf536c06b1fdc4f1e117afd5
win11905 <donothavemoney@gmail.com>
2021-09-05 00:11:27 -0400

    fix grammar

--- src/lib/prism.ts
+++ src/lib/prism.ts
@@ -1,35 +1,35 @@
-import visit from 'unist-util-visit'
-import refractor from 'refractor'
-import nodeToString from 'hast-util-to-string'
+import { visit } from 'unist-util-visit'
+import { refractor } from 'refractor'
+import { toString } from 'hast-util-to-string'
 import rangeParser from 'parse-numeric-range'
 
-const parseOption = (language: string) => {
+const getOptions = (node) => {
   let highlightLines = []
-  let spilitLanguage = language
-  let options = []
+  if (node.data) {
+    const data = node.data.meta
+    if (data.split('{').length > 1) {
+      const options = data.split('{')
+      options.forEach((option) => {
+        option = option.slice(0, -1)
+        const splitOption = option.replace(/ /g, ``).split(`:`)
 
-  if (language.split('{').length > 1) {
-    ;[spilitLanguage, ...options] = language.split('{')
-    options.forEach((option) => {
-      option = option.slice(0, -1)
-      const splitOption = option.replace(/ /g, ``).split(`:`)
-
-      if (splitOption.length === 1 && rangeParser(option).length > 0) {
-        highlightLines = rangeParser(option).filter((n) => n > 0)
-      }
-    })
+        if (splitOption.length === 1 && rangeParser(option).length > 0) {
+          highlightLines = rangeParser(option).filter((n) => n > 0)
+        }
+      })
+    }
   }
+
   return {
-    spilitLanguage,
     highlightLines,
   }
 }
 
-const getLanguageOption = (node) => {
-  const className = node.properties.className || []
+const getLanguage = (node) => {
+  const className = node.properties.className
   for (const classListItem of className) {
     if (classListItem.slice(0, 9) === 'language-') {
-      return parseOption(classListItem.slice(9))
+      return classListItem.slice(9).toLowerCase()
     }
   }
 }
@@ -39,21 +39,34 @@ const piper = (node, index, parent) => {
     return
   }
 
-  const { spilitLanguage, highlightLines } = getLanguageOption(node)
+  if (!node.properties.className) {
+    node.properties.className = ['language-']
+  }
+
+  console.log(node)
+
+  const language = getLanguage(node)
+  const { highlightLines } = getOptions(node)
 
   parent.properties.className = (parent.properties.className || []).concat(
-    'language-' + spilitLanguage
+    'language-' + language
   )
   for (let i = 0; i < node.properties.className.length; ++i) {
     if (node.properties.className[i].slice(0, 9) === 'language-') {
-      node.properties.className = 'language-' + spilitLanguage
+      node.properties.className = 'language-' + language
     }
   }
-  const codeLine = nodeToString(node).split('\n')
+  const codeLine = toString(node).split('\n')
   let result = []
   codeLine.slice(0, -1).forEach((element, index) => {
     if (element === '') element = ' '
-    const childElem = refractor.highlight(element, spilitLanguage)
+    let childElem
+    try {
+      childElem = refractor.highlight(element, language).children
+    } catch {
+      childElem = [{ type: 'text', value: element }]
+    }
+
     if (highlightLines.includes(index + 1)) {
       result.push({
         type: 'element',
