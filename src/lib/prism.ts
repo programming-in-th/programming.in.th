fa403a69c383b87e83d7ae86b1baa79da8b60b0e
win11905 <donothavemoney@gmail.com>
2021-09-09 19:11:54 -0400

    Add lineNumber

--- src/lib/prism.ts
+++ src/lib/prism.ts
@@ -5,6 +5,7 @@ import rangeParser from 'parse-numeric-range'
 
 const getOptions = (node) => {
   let highlightLines = []
+  let lineNumber = false
   if (node.data) {
     const data = node.data.meta
     if (data.split('{').length > 1) {
@@ -16,12 +17,21 @@ const getOptions = (node) => {
         if (splitOption.length === 1 && rangeParser(option).length > 0) {
           highlightLines = rangeParser(option).filter((n) => n > 0)
         }
+
+        if (
+          splitOption.length === 2 &&
+          splitOption[0] === `lineNumber` &&
+          splitOption[1].trim() === `true`
+        ) {
+          lineNumber = true
+        }
       })
     }
   }
 
   return {
     highlightLines,
+    lineNumber,
   }
 }
 
@@ -43,10 +53,8 @@ const piper = (node, index, parent) => {
     node.properties.className = ['language-']
   }
 
-  console.log(node)
-
   const language = getLanguage(node)
-  const { highlightLines } = getOptions(node)
+  const { highlightLines, lineNumber } = getOptions(node)
 
   parent.properties.className = (parent.properties.className || []).concat(
     'language-' + language
@@ -87,8 +95,22 @@ const piper = (node, index, parent) => {
       result = [...result, { type: 'text', value: '\n' }]
     }
   })
-
   node.children = result
+
+  if (lineNumber) {
+    parent.properties.className.push('line-numbers')
+    parent.children.push({
+      type: 'element',
+      tagName: 'span',
+      properties: {
+        className: ['line-numbers-rows'],
+      },
+      children: codeLine.slice(0, -1).map(() => ({
+        type: 'element',
+        tagName: 'span',
+      })),
+    })
+  }
 }
 
 const highlight: () => any = () => {
