862e861d4ec5ae773f77c77f3a2c17d8cde66434
win11905 <donothavemoney@gmail.com>
2021-09-10 09:11:22 -0400

    Revert code display

--- src/components/Code.tsx
+++ src/components/Code.tsx
@@ -1,15 +1,42 @@
-import React, { useEffect, useState } from 'react'
-import { renderMarkdown } from 'lib/renderMarkdown'
+import React from 'react'
+import Highlight, { Prism } from 'prism-react-renderer'
+;((typeof global !== 'undefined' ? global : window) as any).Prism = Prism
 
-export const Code = ({ code, language }) => {
-  const [data, setData] = useState<string>('')
-  useEffect(() => {
-    renderMarkdown(
-      `\`\`\`${language} \{lineNumber: true\}\n${code}\`\`\``
-    ).then((response) => {
-      setData(response)
-    })
-  }, [code, language])
+require('prismjs/components/prism-rust')
+require('prismjs/components/prism-java')
 
-  return <div dangerouslySetInnerHTML={{ __html: data }} />
+export interface CodeBlockProps {
+  code: string
+  language: string
+}
+
+export const Code = ({ code, language }: CodeBlockProps) => {
+  return (
+    <Highlight Prism={Prism} code={code} language={language as any}>
+      {({ style, tokens, getLineProps, getTokenProps }) => (
+        <pre
+          style={{ ...style }}
+          className={`overflow-y-auto	text-xs	language-${language}`}
+        >
+          <code className={`language-${language}`}>
+            {tokens.map((line, i) => (
+              <tr key={i} {...getLineProps({ line, key: i })}>
+                <td
+                  key={i + 'l'}
+                  className="w-8 pr-2 text-gray-400 select-none"
+                >
+                  {i + 1}
+                </td>
+                <td>
+                  {line.map((token, key) => (
+                    <span key={key} {...getTokenProps({ token, key })} />
+                  ))}
+                </td>
+              </tr>
+            ))}
+          </code>
+        </pre>
+      )}
+    </Highlight>
+  )
 }
