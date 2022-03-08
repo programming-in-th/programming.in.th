64f2e17b722f692ee97c5d51da09c2d3fd5670d4
pongsaphol <donothavemoney@gmail.com>
2022-03-07 22:16:51 -0500

    Add tags & menu bar

--- src/pages/tasks.tsx
+++ src/pages/tasks.tsx
@@ -1,13 +1,32 @@
 import React, { useEffect, useState } from 'react'
 import InfiniteScroll from 'react-infinite-scroll-component'
 import { PageLayout } from 'components/Layout'
+import { triggerAsyncId } from 'async_hooks'
 
 const Filter = () => {
   return <></>
 }
 
 const LeftBar = () => {
-  return <></>
+  return (
+    <div className="mx-4 flex w-52 shrink flex-col font-display">
+      <div className="flex h-9 w-full items-center justify-center rounded-md bg-gray-100">
+        <p className="text-sm text-gray-500">All</p>
+      </div>
+      <div className="flex h-9 w-full items-center justify-center rounded-md hover:bg-gray-50">
+        <p className="text-sm text-gray-500">Tried</p>
+      </div>
+      <div className="flex h-9 w-full items-center justify-center rounded-md hover:bg-gray-50">
+        <p className="text-sm text-gray-500">Solved</p>
+      </div>
+      <div className="flex h-9 w-full items-center justify-center rounded-md hover:bg-gray-50">
+        <p className="text-sm text-gray-500">Archives</p>
+      </div>
+      <div className="flex h-9 w-full items-center justify-center rounded-md hover:bg-gray-50">
+        <p className="text-sm text-gray-500">Bookmarked</p>
+      </div>
+    </div>
+  )
 }
 
 type Task = {
@@ -25,53 +44,49 @@ const TaskItem = (context: Task) => {
     <div className="group flex w-full items-center justify-between p-2">
       <div className="flex w-full rounded-xl py-3 px-6 font-display shadow-sm transition group-hover:shadow-md">
         <div className="flex w-full flex-col">
-          <p className="text-sm">{context.title}</p>
+          <p className="text-sm font-medium text-gray-500">{context.title}</p>
           <p className="text-sm text-gray-400">{context.id}</p>
         </div>
-        <div className="flex w-24 items-center justify-center">
-          <p className="text-sm">{context.solved}</p>
+        <div className="flex w-full items-center justify-center">
+          {context.tags.map((tag: string) => (
+            <div className="mx-1 rounded-lg bg-gray-100 px-2 text-sm text-gray-500">
+              {tag}
+            </div>
+          ))}
         </div>
-        <div className="flex h-auto w-36 items-center justify-center">
+        <div className="flex w-full items-center justify-center">
+          <p className="text-sm text-gray-500">{context.solved}</p>
+        </div>
+        <div className="mx-3 flex h-auto w-28 flex-none items-center justify-center">
           <div className="flex h-auto w-full flex-col items-center justify-around">
             <div className="relative h-full w-full">
-              <div className="absolute h-1 w-full rounded-full bg-gray-100" />
-              <div className="absolute h-1 w-1/2 rounded-full bg-gray-500" />
+              <div className="absolute h-1.5 w-full rounded-full bg-gray-100" />
+              <div className="absolute h-1.5 w-1/2 rounded-full bg-gray-500" />
             </div>
             <p className="mt-2 text-sm text-gray-400">{context.score} points</p>
           </div>
         </div>
       </div>
       <div className="w-14 p-4">
-        {bookmark ? (
-          <svg
-            onClick={() => setBookmark(false)}
-            width="18"
-            height="17"
-            viewBox="0 0 18 17"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              d="M8.04894 0.927049C8.3483 0.00573802 9.6517 0.00574017 9.95106 0.927051L11.2451 4.90983C11.379 5.32185 11.763 5.60081 12.1962 5.60081H16.3839C17.3527 5.60081 17.7554 6.84043 16.9717 7.40983L13.5838 9.87132C13.2333 10.126 13.0866 10.5773 13.2205 10.9894L14.5146 14.9721C14.8139 15.8934 13.7595 16.6596 12.9757 16.0902L9.58778 13.6287C9.2373 13.374 8.7627 13.374 8.41221 13.6287L5.02426 16.0902C4.24054 16.6596 3.18607 15.8934 3.48542 14.9721L4.7795 10.9894C4.91338 10.5773 4.76672 10.126 4.41623 9.87132L1.02827 7.40983C0.244561 6.84043 0.647338 5.60081 1.61606 5.60081H5.8038C6.23703 5.60081 6.62099 5.32185 6.75486 4.90983L8.04894 0.927049Z"
-              fill="#64748B"
-            />
-          </svg>
-        ) : (
-          <svg
-            className="hidden group-hover:block"
-            onClick={() => setBookmark(true)}
-            width="18"
-            height="17"
-            viewBox="0 0 18 17"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              d="M8.52447 1.08156C8.67415 0.620905 9.32585 0.620901 9.47553 1.08156L10.7696 5.06434C10.9704 5.68237 11.5464 6.10081 12.1962 6.10081H16.3839C16.8683 6.10081 17.0697 6.72062 16.6778 7.00532L13.2899 9.46681C12.7641 9.84878 12.5442 10.5258 12.745 11.1439L14.0391 15.1266C14.1887 15.5873 13.6615 15.9704 13.2696 15.6857L9.88168 13.2242C9.35595 12.8422 8.64405 12.8422 8.11832 13.2242L4.73037 15.6857C4.33851 15.9704 3.81127 15.5873 3.96095 15.1266L5.25503 11.1439C5.45584 10.5258 5.23585 9.84878 4.71012 9.46681L1.32217 7.00532C0.930313 6.72062 1.1317 6.10081 1.61606 6.10081H5.8038C6.45364 6.10081 7.02958 5.68237 7.23039 5.06434L8.52447 1.08156Z"
-              stroke="#CBD5E1"
-            />
-          </svg>
-        )}
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          onClick={() => setBookmark(!bookmark)}
+          className={`${
+            bookmark
+              ? 'fill-gray-500 stroke-gray-500'
+              : 'hidden stroke-gray-200 group-hover:block'
+          }`}
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+          strokeWidth={2}
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M11.049 2.927c.3-.921 1.603-.921 1.902 0l1.519 4.674a1 1 0 00.95.69h4.915c.969 0 1.371 1.24.588 1.81l-3.976 2.888a1 1 0 00-.363 1.118l1.518 4.674c.3.922-.755 1.688-1.538 1.118l-3.976-2.888a1 1 0 00-1.176 0l-3.976 2.888c-.783.57-1.838-.197-1.538-1.118l1.518-4.674a1 1 0 00-.363-1.118l-3.976-2.888c-.784-.57-.38-1.81.588-1.81h4.914a1 1 0 00.951-.69l1.519-4.674z"
+          />
+        </svg>
       </div>
     </div>
   )
@@ -93,7 +108,7 @@ const Index = () => {
             id: item.id,
             title: item.title,
             tags: item.tags,
-            solved: [],
+            solved: item.solved,
             score: 50,
             fullScore: item.fullScore
           }
@@ -111,24 +126,48 @@ const Index = () => {
   return (
     <>
       <div className="flex h-screen flex-col items-center">
-        <div className="flex w-full flex-col items-center bg-blue-500 py-10 text-white">
-          <p className="text-3xl">Tasks</p>
-          <p className="text-md">browse over 700+ tasks</p>
-          <input className=""></input>
+        <div className="flex w-full flex-col items-center py-16">
+          <p className="text-3xl text-gray-500">Tasks</p>
+          <p className="text-md text-gray-500">browse over 700+ tasks</p>
+          <input
+            className="my-4 w-60 rounded-md border-gray-300 bg-gray-100 py-1 px-2 text-sm shadow-sm"
+            placeholder="search..."
+          ></input>
         </div>
-        <div className="w-full max-w-6xl">
+        <div className="flex w-full max-w-7xl">
           <LeftBar />
-          <InfiniteScroll
-            className="flex w-full flex-col"
-            dataLength={task.length}
-            next={fetchMoreData}
-            hasMore={true}
-            loader={<div className="text-center">Loading...</div>}
-          >
-            {task.map(context => (
-              <TaskItem {...context} />
-            ))}
-          </InfiniteScroll>
+          <div className="h-full w-full">
+            <div className="group flex w-full items-center justify-between px-2">
+              <div className="flex w-full px-6 font-display">
+                <div className="flex w-full flex-col">
+                  <p className="text-sm font-medium text-gray-400">
+                    Problem Title
+                  </p>
+                </div>
+                <div className="flex w-full justify-center">
+                  <p className="text-sm font-medium text-gray-400">Show tag</p>
+                </div>
+                <div className="flex w-full shrink items-center justify-center">
+                  <p className="text-sm text-gray-400">Solved</p>
+                </div>
+                <div className="mx-3 flex h-auto w-28 flex-none items-center justify-center">
+                  <p className="text-sm text-gray-400">Score</p>
+                </div>
+              </div>
+              <div className="w-14 px-4" />
+            </div>
+            <InfiniteScroll
+              className="flex w-full flex-col"
+              dataLength={task.length}
+              next={fetchMoreData}
+              hasMore={true}
+              loader={<div className="text-center">Loading...</div>}
+            >
+              {task.map(context => (
+                <TaskItem {...context} />
+              ))}
+            </InfiniteScroll>
+          </div>
         </div>
       </div>
     </>
