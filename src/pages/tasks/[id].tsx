b1efa068672721a72f7af2a8ae051cde51c735df
Possawat Suksai <markkung08@gmail.com>
2022-07-16 13:07:52 +0700

    Pre-render task

--- src/pages/tasks/[id].tsx
+++ src/pages/tasks/[id].tsx
@@ -1,10 +1,39 @@
-import React, { useEffect, useState } from 'react'
+import React from 'react'
 import { useRouter } from 'next/router'
 
-const Tasks = () => {
+import { GetStaticProps, GetStaticPaths, InferGetStaticPropsType } from 'next'
+
+import prisma from '@/lib/prisma'
+
+const Tasks = ({ task }: InferGetStaticPropsType<typeof getStaticProps>) => {
   const router = useRouter()
-  const { id } = router.query
-  return <p>{id}</p>
+
+  return router.isFallback ? null : <p>{task.id}</p>
 }
 
 export default Tasks
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  const tasks = await prisma.task.findMany()
+
+  const paths = tasks.map(task => ({
+    params: { id: task.id }
+  }))
+
+  return {
+    paths,
+    fallback: true
+  }
+}
+
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+  const task = await prisma.task.findUnique({
+    where: { id: `${params.id}` }
+  })
+
+  return {
+    props: {
+      task
+    }
+  }
+}
