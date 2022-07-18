6a3d0fd57aa0925dfd4431e9738cc7a8c4737821
betich <betich365@gmail.com>
2022-07-18 15:11:21 +0700

    Update Footer

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -5,16 +5,19 @@ import { Footer } from './Footer'
 import { Nav } from './Nav'
 
 export const PageLayout = ({ children }) => {
-  const { pathname } = useRouter()
+  const router = useRouter()
+  const location = useMemo(() => {
+    return router.pathname.split('/')[1]
+  }, [router])
 
   const backgroundColor = useMemo(() => {
-    switch (pathname) {
+    switch (`/${location}`) {
       case '/':
         return '#F8FAFC'
       default:
         return '#FFFFFF'
     }
-  }, [pathname])
+  }, [location])
 
   return (
     <div style={{ backgroundColor }} className="font-display w-full">
