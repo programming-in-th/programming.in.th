3df9aaa5ca8443d9953db4f1083a75fe0ad808e2
betich <betich365@gmail.com>
2022-07-21 13:48:02 +0700

    Add color to provider buttons

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -209,11 +209,29 @@ const Login = () => {
                     <span className="sr-only">Sign in with Google</span>
                     <svg
                       className="h-5 w-5"
-                      aria-hidden="true"
-                      fill="currentColor"
-                      viewBox="0 0 488 512"
+                      viewBox="0 0 24 24"
+                      width="24"
+                      height="24"
+                      xmlns="http://www.w3.org/2000/svg"
                     >
-                      <path d="M488 261.8C488 403.3 391.1 504 248 504 110.8 504 0 393.2 0 256S110.8 8 248 8c66.8 0 123 24.5 166.3 64.9l-67.5 64.9C258.5 52.6 94.3 116.6 94.3 256c0 86.5 69.1 156.6 153.7 156.6 98.2 0 135-70.4 140.8-106.9H248v-85.3h236.1c2.3 12.7 3.9 24.9 3.9 41.4z"></path>
+                      <g transform="matrix(1, 0, 0, 1, 27.009001, -39.238998)">
+                        <path
+                          fill="#4285F4"
+                          d="M -3.264 51.509 C -3.264 50.719 -3.334 49.969 -3.454 49.239 L -14.754 49.239 L -14.754 53.749 L -8.284 53.749 C -8.574 55.229 -9.424 56.479 -10.684 57.329 L -10.684 60.329 L -6.824 60.329 C -4.564 58.239 -3.264 55.159 -3.264 51.509 Z"
+                        />
+                        <path
+                          fill="#34A853"
+                          d="M -14.754 63.239 C -11.514 63.239 -8.804 62.159 -6.824 60.329 L -10.684 57.329 C -11.764 58.049 -13.134 58.489 -14.754 58.489 C -17.884 58.489 -20.534 56.379 -21.484 53.529 L -25.464 53.529 L -25.464 56.619 C -23.494 60.539 -19.444 63.239 -14.754 63.239 Z"
+                        />
+                        <path
+                          fill="#FBBC05"
+                          d="M -21.484 53.529 C -21.734 52.809 -21.864 52.039 -21.864 51.239 C -21.864 50.439 -21.724 49.669 -21.484 48.949 L -21.484 45.859 L -25.464 45.859 C -26.284 47.479 -26.754 49.299 -26.754 51.239 C -26.754 53.179 -26.284 54.999 -25.464 56.619 L -21.484 53.529 Z"
+                        />
+                        <path
+                          fill="#EA4335"
+                          d="M -14.754 43.989 C -12.984 43.989 -11.404 44.599 -10.154 45.789 L -6.734 42.369 C -8.804 40.429 -11.514 39.239 -14.754 39.239 C -19.444 39.239 -23.494 41.939 -25.464 45.859 L -21.484 48.949 C -20.534 46.099 -17.884 43.989 -14.754 43.989 Z"
+                        />
+                      </g>
                     </svg>
                   </button>
                 </div>
@@ -222,7 +240,7 @@ const Login = () => {
                   <button className="inline-flex w-full justify-center rounded-md border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-500 shadow-sm hover:bg-gray-50">
                     <span className="sr-only">Sign in with Facebook</span>
                     <svg
-                      className="h-5 w-5"
+                      className="h-5 w-5 text-prog-primary-500"
                       aria-hidden="true"
                       fill="currentColor"
                       viewBox="0 0 20 20"
@@ -243,7 +261,7 @@ const Login = () => {
                   >
                     <span className="sr-only">Sign in with GitHub</span>
                     <svg
-                      className="h-5 w-5"
+                      className="h-5 w-5 text-gray-900"
                       aria-hidden="true"
                       fill="currentColor"
                       viewBox="0 0 20 20"
