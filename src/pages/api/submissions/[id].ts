7ca739e06a0b7296cd7dc1e3493d1086366b76e7
Possawat Suksai <markkung08@gmail.com>
2022-07-22 16:45:44 +0700

    fix: change handler name

--- src/pages/api/submissions/[id].ts
+++ src/pages/api/submissions/[id].ts
@@ -1,7 +1,7 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
 import prisma from '@/lib/prisma'
 
-export default async function taskHandler(
+export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
