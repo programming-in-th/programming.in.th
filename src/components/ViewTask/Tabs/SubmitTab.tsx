aec424e85151dc44048ba6631d84779bfaa67c8d
betich <betich365@gmail.com>
2022-07-25 01:00:22 +0700

    feat: add auth checking in submit page

--- src/components/ViewTask/Tabs/SubmitTab.tsx
+++ src/components/ViewTask/Tabs/SubmitTab.tsx
@@ -1,13 +1,25 @@
 import { Task } from '@prisma/client'
+import { useSession } from 'next-auth/react'
 import { FC } from 'react'
 import { SubmitElement } from '../Elements/SubmitElement'
 
 const SubmitTab: FC<{ task: Task }> = ({ task }) => {
-  return (
-    <div>
-      <SubmitElement task={task} />
-    </div>
-  )
+  const { status } = useSession()
+
+  if (status !== 'authenticated') {
+    return (
+      <div className="flex items-center justify-center h-24">
+        <p className="text-prog-gray-500 text-center">
+          You must be logged in to be to submit
+        </p>
+      </div>
+    )
+  } else
+    return (
+      <div>
+        <SubmitElement task={task} />
+      </div>
+    )
 }
 
 export default SubmitTab
