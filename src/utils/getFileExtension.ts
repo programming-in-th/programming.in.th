fc571da688d3ecfc1e08e24075bce1f1444b22b3
betich <betich365@gmail.com>
2022-08-03 20:41:28 +0700

    fix: fix getFileExtension bug

--- src/utils/getFileExtension.ts
+++ src/utils/getFileExtension.ts
@@ -1,8 +1,8 @@
 export const getLanguage = (fileName: string) => {
-  const result = (fileName
-    .toLowerCase()
-    .replace('.', '')
-    .match(/[0-9a-z]+$/i) ?? [])[0]
+  const result = (fileName.toLowerCase().match(/[0-9a-z]+$/i) ?? [])[0].replace(
+    '.',
+    ''
+  )
 
   if (result === 'c') return 'cpp'
   else return result
