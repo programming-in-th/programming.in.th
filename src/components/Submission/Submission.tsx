4b45e0ebcbdd4d6961dd9d7234022d9c86ca3ee9
Possawat Suksai <markkung08@gmail.com>
2022-08-06 15:21:41 +0700

    chore: run eslint

--- src/components/Submission/Submission.tsx
+++ src/components/Submission/Submission.tsx
@@ -1,5 +1,3 @@
-import { Suspense } from 'react'
-
 import dynamic from 'next/dynamic'
 
 import { Task } from '@prisma/client'
@@ -8,8 +6,8 @@ import dayjs from 'dayjs'
 import { Loading } from '@/components/Loading'
 import useSubmissionData from '@/lib/useSubmissionData'
 
-import { SubmissionGroup } from './Group'
 import { CodeSkeleton } from '../Code'
+import { SubmissionGroup } from './Group'
 
 const DynamicCode = dynamic(() => import('../Code'), {
   suspense: true
--- src/components/Task/LeftBar.tsx
+++ src/components/Task/LeftBar.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useMemo, useState } from 'react'
+import { useMemo } from 'react'
 
 import Link from 'next/link'
 
