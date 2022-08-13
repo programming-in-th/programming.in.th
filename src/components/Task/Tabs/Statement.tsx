faef2d9b20e4aa18a1860919b83c4be9869e771f
Possawat Suksai <markkung08@gmail.com>
2022-08-13 15:17:55 +0700

    fix: pdf rendering in safari

--- src/components/Task/Tabs/Statement.tsx
+++ src/components/Task/Tabs/Statement.tsx
@@ -1,16 +1,19 @@
 import { Task } from '@prisma/client'
 
 const StatementTab = ({ task }: { task: Task }) => {
-  const pdfURL = `${process.env.NEXT_PUBLIC_AWS_URL}/statements/pdf/${task.id}.pdf`
+  const encoded = encodeURIComponent(
+    `${process.env.NEXT_PUBLIC_AWS_URL}/statements/pdf/${task.id}.pdf`
+  )
+  const pdfURL = `https://drive.google.com/viewerng/viewer?embedded=true&url=${encoded}`
 
   return (
     <article className="h-screen">
-      <object data={pdfURL} type="application/pdf" width="100%" height="100%">
+      <iframe src={pdfURL} width="100%" height="100%">
         <p>
           Your web browser doesn&apos;t have a PDF plugin. Instead you can{' '}
           <a href={pdfURL}>click here to download the PDF file.</a>
         </p>
-      </object>
+      </iframe>
     </article>
   )
 }
