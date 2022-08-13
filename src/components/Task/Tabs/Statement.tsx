dbefc20882cca8c732a31671b671e55819365f9f
Possawat Suksai <markkung08@gmail.com>
2022-08-13 14:50:05 +0700

    feat: switch to native PDF embedding

--- src/components/Task/Tabs/Statement.tsx
+++ src/components/Task/Tabs/Statement.tsx
@@ -1,16 +1,17 @@
 import { Task } from '@prisma/client'
 
-import PDFViewer from '../PDFViewer'
-
 const StatementTab = ({ task }: { task: Task }) => {
   const pdfURL = `${process.env.NEXT_PUBLIC_AWS_URL}/statements/pdf/${task.id}.pdf`
 
   return (
-    <div className="">
-      <article className="h-screen">
-        <PDFViewer fileUrl={pdfURL} keywords={[]} />
-      </article>
-    </div>
+    <article className="h-screen">
+      <object data={pdfURL} type="application/pdf" width="100%" height="100%">
+        <p>
+          Your web browser doesn't have a PDF plugin. Instead you can{' '}
+          <a href={pdfURL}>click here to download the PDF file.</a>
+        </p>
+      </object>
+    </article>
   )
 }
 
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,24 +1,18 @@
-import { Worker } from '@react-pdf-viewer/core'
 import { SessionProvider } from 'next-auth/react'
 import { ThemeProvider } from 'next-themes'
 import '@/styles/index.css'
 import '@/styles/fonts.css'
 import '@/styles/style.scss'
 
-import '@react-pdf-viewer/core/lib/styles/index.css'
-import '@react-pdf-viewer/default-layout/lib/styles/index.css'
-
 export default function App({
   Component,
   pageProps: { session, ...pageProps }
 }) {
   return (
-    <Worker workerUrl="https://unpkg.com/pdfjs-dist@2.14.305/build/pdf.worker.js">
-      <SessionProvider session={session}>
-        <ThemeProvider>
-          <Component {...pageProps} />
-        </ThemeProvider>
-      </SessionProvider>
-    </Worker>
+    <SessionProvider session={session}>
+      <ThemeProvider>
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </SessionProvider>
   )
 }
