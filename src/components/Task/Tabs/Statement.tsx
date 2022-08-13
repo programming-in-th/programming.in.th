63631f51fb236bacd41bb4425eaa91bb1a208ec1
Possawat Suksai <markkung08@gmail.com>
2022-08-13 15:31:26 +0700

    style: iframe content

--- src/components/Task/Tabs/Statement.tsx
+++ src/components/Task/Tabs/Statement.tsx
@@ -1,19 +1,13 @@
 import { Task } from '@prisma/client'
 
 const StatementTab = ({ task }: { task: Task }) => {
-  const encoded = encodeURIComponent(
+  const pdfURL = `https://drive.google.com/viewerng/viewer?embedded=true&url=${encodeURIComponent(
     `${process.env.NEXT_PUBLIC_AWS_URL}/statements/pdf/${task.id}.pdf`
-  )
-  const pdfURL = `https://drive.google.com/viewerng/viewer?embedded=true&url=${encoded}`
+  )}`
 
   return (
     <article className="h-screen">
-      <iframe src={pdfURL} width="100%" height="100%">
-        <p>
-          Your web browser doesn&apos;t have a PDF plugin. Instead you can{' '}
-          <a href={pdfURL}>click here to download the PDF file.</a>
-        </p>
-      </iframe>
+      <iframe src={pdfURL} width="100%" height="100%" />
     </article>
   )
 }
