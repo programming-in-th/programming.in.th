6d8589879311771f296d5bdf94b4426f4554ca35
pongsaphol <donothavemoney@gmail.com>
2022-08-14 14:57:42 +0700

    feat: complete pagination

---
+++ src/components/Tasks/Pagination.tsx
@@ -0,0 +1,145 @@
+import Link from 'next/link'
+import { useRouter } from 'next/router'
+
+import clsx from 'clsx'
+
+export const Pagination = ({
+  page,
+  pageLimit
+}: {
+  page: number
+  pageLimit: number
+}) => {
+  const { query, push } = useRouter()
+  return (
+    <div className="justfiy-between mt-4 flex w-full items-center pr-4 pl-4 md:pr-20">
+      <div className="w-28">
+        {page > 1 && (
+          <Link
+            href={{ pathname: '/tasks', query: { ...query, page: page - 1 } }}
+            passHref
+            scroll={false}
+          >
+            <a className="flex items-center text-gray-400">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-4 w-4"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+                strokeWidth={2}
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M10 19l-7-7m0 0l7-7m-7 7h18"
+                />
+              </svg>
+              <p className="ml-2 text-sm">Previous</p>
+            </a>
+          </Link>
+        )}
+      </div>
+      <div className="flex w-full justify-center text-gray-400 md:space-x-6">
+        {Array.from(Array(pageLimit + 1).keys())
+          .slice(1)
+          .reduce((pre, index) => {
+            const component = (
+              <Link
+                key={index}
+                href={{
+                  pathname: '/tasks',
+                  query: { ...query, page: index }
+                }}
+                passHref
+                scroll={false}
+              >
+                <a className="hidden md:block">
+                  <p
+                    className={clsx(
+                      'flex h-7 w-7 items-center justify-center rounded-full transition hover:bg-gray-100',
+                      index === page && 'bg-blue-100 text-blue-600'
+                    )}
+                  >
+                    {index}
+                  </p>
+                </a>
+              </Link>
+            )
+            const blankComponent = (
+              <p className="flex h-7 w-7 items-center justify-center">...</p>
+            )
+            if (pageLimit <= 8) {
+              return [...pre, component]
+            }
+
+            if (page <= 3 || page + 3 > pageLimit) {
+              if (index <= 4 || index + 4 > pageLimit) {
+                return [...pre, component]
+              }
+              if (index === 5) {
+                return [...pre, blankComponent]
+              }
+              return pre
+            }
+
+            if (
+              index === 1 ||
+              index === pageLimit ||
+              (index >= page - 2 && index <= page + 2)
+            ) {
+              return [...pre, component]
+            }
+            if (index === 2 || index === pageLimit - 1) {
+              return [...pre, blankComponent]
+            }
+            return pre
+          }, [])}
+        <select
+          className="px-4 py-2 md:hidden"
+          onChange={({ target: { value } }) =>
+            push({
+              pathname: '/tasks',
+              query: { ...query, page: value }
+            })
+          }
+        >
+          {Array.from(Array(pageLimit + 1).keys())
+            .slice(1)
+            .map(index => (
+              <option value={index} key={index} selected={index === page}>
+                {index}
+              </option>
+            ))}
+        </select>
+      </div>
+      <div className="w-28">
+        {page < pageLimit && (
+          <Link
+            href={{ pathname: '/tasks', query: { ...query, page: page + 1 } }}
+            passHref
+            scroll={false}
+          >
+            <a className="flex items-center justify-end text-gray-400">
+              <p className="mr-2 text-sm">Next</p>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-4 w-4"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+                strokeWidth={2}
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M14 5l7 7m0 0l-7 7m7-7H3"
+                />
+              </svg>
+            </a>
+          </Link>
+        )}
+      </div>
+    </div>
+  )
+}
--- src/components/Tasks/RightDisplay.tsx
+++ src/components/Tasks/RightDisplay.tsx
@@ -5,7 +5,7 @@ import { useRouter } from 'next/router'
 import { IGeneralTask } from '@/types/tasks'
 
 import { AllTasks } from './All'
-import Link from 'next/link'
+import { Pagination } from './Pagination'
 
 const ComingSoonTab = () => {
   return (
@@ -80,71 +80,6 @@ export const RightDisplay = ({
     <div className="flex w-full flex-col">
       <AllTasks tasks={slicedTask} tag={tag} setTag={setTag} />
       <Pagination page={page} pageLimit={pageLimit} />
-    </div>
-  )
-}
-
-const Pagination = ({
-  page,
-  pageLimit
-}: {
-  page: number
-  pageLimit: number
-}) => {
-  const { query } = useRouter()
-  return (
-    <div className="justfiy-between mt-4 flex w-full pr-4 pl-4 md:pr-20">
-      {page > 1 && (
-        <Link
-          href={{ pathname: '/tasks', query: { ...query, page: page - 1 } }}
-          passHref
-          scroll={false}
-        >
-          <a className="flex items-center text-gray-400">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-4 w-4"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-              strokeWidth={2}
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M10 19l-7-7m0 0l7-7m-7 7h18"
-              />
-            </svg>
-            <p className="ml-2 text-sm">Previous</p>
-          </a>
-        </Link>
-      )}
-      <div className="flex w-full"></div>
-      {page < pageLimit && (
-        <Link
-          href={{ pathname: '/tasks', query: { ...query, page: page + 1 } }}
-          passHref
-          scroll={false}
-        >
-          <a className="flex items-center text-gray-400">
-            <p className="mr-2 text-sm">Next</p>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-4 w-4"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-              strokeWidth={2}
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M14 5l7 7m0 0l-7 7m7-7H3"
-              />
-            </svg>
-          </a>
-        </Link>
-      )}
     </div>
   )
 }
