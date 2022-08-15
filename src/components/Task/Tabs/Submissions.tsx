c6e2fed084196445b0144a13fa145a52121606c6
pongsaphol <donothavemoney@gmail.com>
2022-08-15 22:09:29 +0700

    style: use flexbox in submissions

--- src/components/Task/Tabs/Submissions.tsx
+++ src/components/Task/Tabs/Submissions.tsx
@@ -4,31 +4,88 @@ import { Task } from '@prisma/client'
 import dayjs from 'dayjs'
 
 import useSubmissionList from '@/lib/useSubmissionList'
+import clsx from 'clsx'
+import { IGeneralSubmission } from '@/types/submissions'
 
 const Columns = [
   {
-    title: 'Submission Time',
-    field: 'submittedAt'
+    title: 'Time',
+    field: 'submittedAt',
+    width: 'w-[10rem]',
+    child: (sub: IGeneralSubmission) => {
+      const dt = new Date(sub.submittedAt)
+      return (
+        <div className="flex flex-col">
+          <p className="font-medium">{dayjs(dt).format('DD MMM YYYY')}</p>
+          <p className="text-gray-400 dark:text-gray-300">
+            {dayjs(dt).format('HH:mm:ss')}
+          </p>
+        </div>
+      )
+    }
   },
   {
     title: 'Name',
-    field: 'userId'
+    field: 'userId',
+    width: 'w-[16rem]',
+    child: (sub: IGeneralSubmission) => (
+      <p className="truncate text-center font-medium">{sub.user?.username}</p>
+    )
   },
   {
     title: 'Score',
-    field: 'score'
+    field: 'score',
+    width: 'w-[10rem]',
+    child: (sub: IGeneralSubmission, task: Task) => (
+      <div className="flex h-auto w-28 items-center justify-center">
+        <div className="flex h-auto w-full flex-col items-center justify-around">
+          <div className="relative h-full w-full">
+            <div className="absolute h-1.5 w-full rounded-full bg-gray-100" />
+            <div
+              className={`absolute h-1.5 rounded-full ${
+                sub.score === task.fullScore ? 'bg-blue-500' : 'bg-gray-500'
+              }`}
+              style={{
+                width: `${(sub.score / 100) * 100}%`
+              }}
+            />
+          </div>
+          <p className="mt-2 text-xs text-gray-500 dark:text-gray-200">
+            {sub.score} points
+          </p>
+        </div>
+      </div>
+    )
   },
   {
     title: 'Language',
-    field: 'language'
+    field: 'language',
+    width: 'w-[6rem]',
+    child: (sub: IGeneralSubmission) => (
+      <p className="text-center font-medium">{sub.language}</p>
+    )
   },
   {
     title: 'Time',
-    field: 'time'
+    field: 'time',
+    width: 'w-[6rem]',
+    child: (sub: IGeneralSubmission) => (
+      <p className="text-center font-medium">
+        {sub.time}{' '}
+        <span className="font-light text-gray-400 dark:text-gray-300">ms</span>
+      </p>
+    )
   },
   {
     title: 'Memory',
-    field: 'memory'
+    field: 'memory',
+    width: 'w-[6rem]',
+    child: (sub: IGeneralSubmission) => (
+      <p className="text-center font-medium">
+        {sub.memory}{' '}
+        <span className="font-light text-gray-400 dark:text-gray-300">kB</span>
+      </p>
+    )
   }
 ]
 
@@ -37,116 +94,35 @@ const SubmissionsTab = ({ task }: { task: Task }) => {
     useSubmissionList(task.id)
 
   return (
-    <div>
-      <table className="w-full table-auto border-separate border-spacing-y-3 text-sm">
-        <thead className="text-gray-500 dark:text-white">
-          <tr>
-            {Columns.map(({ title, field }) => (
-              <th key={field} className="py-2 text-center font-light">
-                <button className="group flex w-full items-center justify-center gap-1">
-                  <p className="text-gray-500 transition-colors group-hover:text-gray-600 dark:text-gray-100 dark:group-hover:text-gray-300">
-                    {title}
-                  </p>
-
-                  {/* <ChevronUpIcon className="h-3 w-3 text-gray-400 transition-colors group-hover:text-gray-500 dark:text-gray-50 dark:group-hover:text-gray-300" /> */}
-                </button>
-              </th>
-            ))}
-          </tr>
-        </thead>
-        <tbody className="text-gray-500 dark:text-white">
-          {submissions.map(sub => {
-            const dt = new Date(sub.submittedAt)
-            return (
-              <tr
-                key={sub.id}
-                className="bg-white shadow-md transition-colors hover:bg-slate-50 dark:bg-slate-700 dark:hover:bg-slate-600"
-              >
-                <td className="px-6 py-4">
-                  <Link href={`/submissions/${sub.id}`} passHref>
-                    <a>
-                      <div className="flex flex-col">
-                        <p className="font-medium">
-                          {dayjs(dt).format('DD MMM YYYY')}
-                        </p>
-                        <p className="text-gray-400 dark:text-gray-300">
-                          {dayjs(dt).format('HH:mm:ss')}
-                        </p>
-                      </div>
-                    </a>
-                  </Link>
-                </td>
-                <td className="py-4">
-                  <Link href={`/submissions/${sub.id}`} passHref>
-                    <a>
-                      <p className="text-center font-medium">
-                        {sub.user?.username}
-                      </p>
-                    </a>
-                  </Link>
-                </td>
-                <td className="py-4">
-                  <Link href={`/submissions/${sub.id}`} passHref>
-                    <a>
-                      <div className="mx-auto flex h-auto w-28 items-center justify-center">
-                        <div className="flex h-auto w-full flex-col items-center justify-around">
-                          <div className="relative h-full w-full">
-                            <div className="absolute h-1.5 w-full rounded-full bg-gray-100" />
-                            <div
-                              className={`absolute h-1.5 rounded-full ${
-                                sub.score === task.fullScore
-                                  ? 'bg-blue-500'
-                                  : 'bg-gray-500'
-                              }`}
-                              style={{
-                                width: `${(sub.score / 100) * 100}%`
-                              }}
-                            />
-                          </div>
-                          <p className="mt-2 text-xs text-gray-500 dark:text-gray-200">
-                            {sub.score} points
-                          </p>
-                        </div>
-                      </div>
-                    </a>
-                  </Link>
-                </td>
-                <td className="py-4">
-                  <Link href={`/submissions/${sub.id}`} passHref>
-                    <a>
-                      <p className="text-center font-medium">{sub.language}</p>
-                    </a>
-                  </Link>
-                </td>
-                <td className="py-4">
-                  <Link href={`/submissions/${sub.id}`} passHref>
-                    <a>
-                      <p className="text-center font-medium">
-                        {sub.time}{' '}
-                        <span className="font-light text-gray-400 dark:text-gray-300">
-                          ms
-                        </span>
-                      </p>
-                    </a>
-                  </Link>
-                </td>
-                <td className="px-6 py-4">
-                  <Link href={`/submissions/${sub.id}`} passHref>
-                    <a>
-                      <p className="text-center font-medium">
-                        {sub.memory}{' '}
-                        <span className="font-light text-gray-400 dark:text-gray-300">
-                          kb
-                        </span>
-                      </p>
-                    </a>
-                  </Link>
-                </td>
-              </tr>
-            )
-          })}
-        </tbody>
-      </table>
+    <div className="flex flex-shrink flex-col">
+      <div className="flex w-full">
+        {Columns.map(column => (
+          <p
+            key={column.field}
+            className={clsx('flex justify-center text-sm', column.width)}
+          >
+            {column.title}
+          </p>
+        ))}
+      </div>
+      <div className="mt-2 flex flex-col space-y-2">
+        {submissions.map(sub => (
+          <Link href={`/submissions/${sub.id}`} passHref key={sub.id}>
+            <a className="flex w-full rounded-xl py-3 font-display shadow-md transition hover:shadow-lg dark:bg-slate-700">
+              {Columns.map(column => (
+                <div
+                  className={clsx(
+                    'flex min-w-0 items-center justify-center text-sm',
+                    column.width
+                  )}
+                >
+                  {column.child(sub, task)}
+                </div>
+              ))}
+            </a>
+          </Link>
+        ))}
+      </div>
       <button
         disabled={isLoadingMore || isReachingEnd}
         onClick={() => setSize(size + 1)}
