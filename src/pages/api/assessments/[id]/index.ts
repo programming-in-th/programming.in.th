f84e069e549b9dc109344b1efced3feded4f22bc
Possawat Suksai <markkung08@gmail.com>
2022-08-21 20:46:15 +0700

    feat: implement acl for assessment

--- src/pages/api/assessments/[id]/index.ts
+++ src/pages/api/assessments/[id]/index.ts
@@ -3,24 +3,43 @@ import type { NextApiRequest, NextApiResponse } from 'next'
 import { unstable_getServerSession } from 'next-auth'
 
 import prisma from '@/lib/prisma'
-import { methodNotAllowed, unauthorized, ok } from '@/utils/response'
+import {
+  methodNotAllowed,
+  unauthorized,
+  ok,
+  badRequest
+} from '@/utils/response'
 
 import { authOptions } from '../../auth/[...nextauth]'
+import { IndividualAssessmentSchema } from '@/lib/api/schema/assessment'
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
   if (req.method === 'GET') {
+    const { query } = req
+    const parsedQuery = IndividualAssessmentSchema.safeParse(query)
+
+    if (!parsedQuery.success) {
+      return badRequest(res)
+    }
+
+    const { id } = parsedQuery.data
     const session = await unstable_getServerSession(req, res, authOptions)
 
     if (!session) {
       return unauthorized(res)
     }
 
-    const assessment = await prisma.assessment.findUnique({
+    const assessment = await prisma.assessment.findFirst({
       where: {
-        id: String(req.query.id)
+        id,
+        users: {
+          some: {
+            userId: session.user.id!
+          }
+        }
       }
     })
 
