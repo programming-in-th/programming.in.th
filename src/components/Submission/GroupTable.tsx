5c54806620e664d7bfc72b294dc37b6bcbf7ef46
NakZaa <nak11494@gmail.com>
2022-08-26 14:30:19 +0700

    feat: combine collapse all and expand all button

--- src/components/Submission/GroupTable.tsx
+++ src/components/Submission/GroupTable.tsx
@@ -1,11 +1,21 @@
 import { useState } from 'react'
 
+import { CollapseAll, ExpandAll } from '@/svg/ExpandCollapse'
+
 import { IGroup } from '@/types/submissions'
 
 import { Accordion } from './Accordian'
 
 const GroupTable = ({ groups }: { groups: IGroup[] }) => {
   const [open, setOpen] = useState(false)
+  const [title, setTitle] = useState('expand all')
+  const collapseAll = () => {
+    setOpen(!open)
+    setTitle(state => {
+      return state === 'expand all' ? 'collapse all' : 'expand all'
+    })
+  }
+
   return (
     <>
       <div className="flex w-full justify-between">
@@ -14,46 +24,12 @@ const GroupTable = ({ groups }: { groups: IGroup[] }) => {
           <button
             className="flex"
             onClick={() => {
-              setOpen(false)
+              collapseAll()
             }}
           >
-            <p className="dark:text-white">collapse all</p>
+            <p className="dark:text-white">{title}</p>
             <div className="flex h-full items-center justify-center text-[#64748B] dark:text-white">
-              <svg
-                className="mx-2"
-                width="11"
-                height="11"
-                viewBox="0 0 9 9"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <rect width="9" height="9" fill="currentColor" />
-              </svg>
-            </div>
-          </button>
-          <button
-            className="flex"
-            onClick={() => {
-              setOpen(true)
-            }}
-          >
-            <p className="dark:text-white">expand all</p>
-            <div className="flex h-full items-center justify-center text-[#64748B] dark:text-white">
-              <svg
-                className="mx-2"
-                width="13"
-                height="13"
-                viewBox="0 0 10 10"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  fillRule="evenodd"
-                  clipRule="evenodd"
-                  d="M1.5 2C1.5 1.72386 1.72386 1.5 2 1.5H8C8.27614 1.5 8.5 1.72386 8.5 2C8.5 2.27614 8.27614 2.5 8 2.5H2C1.72386 2.5 1.5 2.27614 1.5 2ZM1.5 4C1.5 3.72386 1.72386 3.5 2 3.5H8C8.27614 3.5 8.5 3.72386 8.5 4C8.5 4.27614 8.27614 4.5 8 4.5H2C1.72386 4.5 1.5 4.27614 1.5 4ZM1.5 6C1.5 5.72386 1.72386 5.5 2 5.5H8C8.27614 5.5 8.5 5.72386 8.5 6C8.5 6.27614 8.27614 6.5 8 6.5H2C1.72386 6.5 1.5 6.27614 1.5 6ZM1.5 8C1.5 7.72386 1.72386 7.5 2 7.5H8C8.27614 7.5 8.5 7.72386 8.5 8C8.5 8.27614 8.27614 8.5 8 8.5H2C1.72386 8.5 1.5 8.27614 1.5 8Z"
-                  fill="currentColor"
-                />
-              </svg>
+              <i>{open === false ? <ExpandAll /> : <CollapseAll />}</i>
             </div>
           </button>
         </div>
