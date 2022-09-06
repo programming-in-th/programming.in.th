a72e4a2fb816ac958e682639ad00ac7faaad463d
iammarkps <markkung08@gmail.com>
2022-09-06 12:58:32 +0700

    feat: generate database migration for consistency

---
+++ prisma/migrations/20220906020209_init/migration.sql
@@ -0,0 +1,276 @@
+-- CreateEnum
+CREATE TYPE "StatementType" AS ENUM ('PDF', 'MARKDOWN');
+
+-- CreateEnum
+CREATE TYPE "TaskType" AS ENUM ('NORMAL', 'COMMUNICATION', 'OUTPUT_ONLY');
+
+-- CreateTable
+CREATE TABLE "Account" (
+    "id" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
+    "type" TEXT NOT NULL,
+    "provider" TEXT NOT NULL,
+    "providerAccountId" TEXT NOT NULL,
+    "refresh_token" TEXT,
+    "access_token" TEXT,
+    "expires_at" INTEGER,
+    "token_type" TEXT,
+    "scope" TEXT,
+    "id_token" TEXT,
+    "session_state" TEXT,
+    "oauth_token_secret" TEXT,
+    "oauth_token" TEXT,
+
+    CONSTRAINT "Account_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Session" (
+    "id" TEXT NOT NULL,
+    "sessionToken" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
+    "expires" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Session_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL,
+    "username" TEXT,
+    "name" TEXT,
+    "email" TEXT,
+    "emailVerified" TIMESTAMP(3),
+    "image" TEXT,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "admin" BOOLEAN NOT NULL DEFAULT false,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "VerificationToken" (
+    "identifier" TEXT NOT NULL,
+    "token" TEXT NOT NULL,
+    "expires" TIMESTAMP(3) NOT NULL
+);
+
+-- CreateTable
+CREATE TABLE "Bookmark" (
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "userId" TEXT NOT NULL,
+    "taskId" TEXT NOT NULL,
+
+    CONSTRAINT "Bookmark_pkey" PRIMARY KEY ("taskId","userId")
+);
+
+-- CreateTable
+CREATE TABLE "Task" (
+    "id" TEXT NOT NULL,
+    "title" TEXT NOT NULL,
+    "fullScore" INTEGER NOT NULL,
+    "private" BOOLEAN NOT NULL DEFAULT false,
+    "type" "TaskType" NOT NULL DEFAULT 'NORMAL',
+    "statement" "StatementType" NOT NULL DEFAULT 'PDF',
+    "categoryId" TEXT,
+
+    CONSTRAINT "Task_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Category" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "childCategoryId" TEXT NOT NULL,
+    "parentCategoryId" TEXT NOT NULL,
+
+    CONSTRAINT "Category_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Submission" (
+    "id" SERIAL NOT NULL,
+    "taskId" TEXT NOT NULL,
+    "status" TEXT NOT NULL DEFAULT 'In Queue',
+    "submittedAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "time" INTEGER NOT NULL DEFAULT 0,
+    "memory" INTEGER NOT NULL DEFAULT 0,
+    "code" BYTEA NOT NULL,
+    "score" INTEGER NOT NULL DEFAULT 0,
+    "groups" JSONB NOT NULL DEFAULT '[]',
+    "language" TEXT NOT NULL,
+    "private" BOOLEAN NOT NULL DEFAULT false,
+    "userId" TEXT,
+    "assessmentId" TEXT,
+
+    CONSTRAINT "Submission_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Assessment" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "archived" BOOLEAN NOT NULL DEFAULT false,
+    "description" TEXT,
+    "instruction" TEXT,
+    "open" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "close" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Assessment_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "TaskOnAssessment" (
+    "taskId" TEXT NOT NULL,
+    "assessmentId" TEXT NOT NULL,
+
+    CONSTRAINT "TaskOnAssessment_pkey" PRIMARY KEY ("taskId","assessmentId")
+);
+
+-- CreateTable
+CREATE TABLE "UserOnAssessment" (
+    "userId" TEXT NOT NULL,
+    "assessmentId" TEXT NOT NULL,
+
+    CONSTRAINT "UserOnAssessment_pkey" PRIMARY KEY ("userId","assessmentId")
+);
+
+-- CreateTable
+CREATE TABLE "OwnerOnAssessment" (
+    "userId" TEXT NOT NULL,
+    "assessmentId" TEXT NOT NULL,
+
+    CONSTRAINT "OwnerOnAssessment_pkey" PRIMARY KEY ("userId","assessmentId")
+);
+
+-- CreateTable
+CREATE TABLE "Tag" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+
+    CONSTRAINT "Tag_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "_TagToTask" (
+    "A" INTEGER NOT NULL,
+    "B" TEXT NOT NULL
+);
+
+-- CreateIndex
+CREATE INDEX "Account_userId_idx" ON "Account"("userId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Account_provider_providerAccountId_key" ON "Account"("provider", "providerAccountId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Session_sessionToken_key" ON "Session"("sessionToken");
+
+-- CreateIndex
+CREATE INDEX "Session_userId_idx" ON "Session"("userId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_username_key" ON "User"("username");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "VerificationToken_token_key" ON "VerificationToken"("token");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "VerificationToken_identifier_token_key" ON "VerificationToken"("identifier", "token");
+
+-- CreateIndex
+CREATE INDEX "Task_id_idx" ON "Task"("id");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Category_childCategoryId_key" ON "Category"("childCategoryId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Category_parentCategoryId_key" ON "Category"("parentCategoryId");
+
+-- CreateIndex
+CREATE INDEX "Submission_id_idx" ON "Submission"("id");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Assessment_name_key" ON "Assessment"("name");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Tag_name_key" ON "Tag"("name");
+
+-- CreateIndex
+CREATE INDEX "Tag_id_idx" ON "Tag"("id");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "_TagToTask_AB_unique" ON "_TagToTask"("A", "B");
+
+-- CreateIndex
+CREATE INDEX "_TagToTask_B_index" ON "_TagToTask"("B");
+
+-- AddForeignKey
+ALTER TABLE "Account" ADD CONSTRAINT "Account_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Session" ADD CONSTRAINT "Session_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Bookmark" ADD CONSTRAINT "Bookmark_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Bookmark" ADD CONSTRAINT "Bookmark_taskId_fkey" FOREIGN KEY ("taskId") REFERENCES "Task"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Task" ADD CONSTRAINT "Task_categoryId_fkey" FOREIGN KEY ("categoryId") REFERENCES "Category"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Category" ADD CONSTRAINT "Category_childCategoryId_fkey" FOREIGN KEY ("childCategoryId") REFERENCES "Category"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Submission" ADD CONSTRAINT "Submission_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Submission" ADD CONSTRAINT "Submission_assessmentId_fkey" FOREIGN KEY ("assessmentId") REFERENCES "Assessment"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Submission" ADD CONSTRAINT "Submission_taskId_fkey" FOREIGN KEY ("taskId") REFERENCES "Task"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "TaskOnAssessment" ADD CONSTRAINT "TaskOnAssessment_taskId_fkey" FOREIGN KEY ("taskId") REFERENCES "Task"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "TaskOnAssessment" ADD CONSTRAINT "TaskOnAssessment_assessmentId_fkey" FOREIGN KEY ("assessmentId") REFERENCES "Assessment"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "UserOnAssessment" ADD CONSTRAINT "UserOnAssessment_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "UserOnAssessment" ADD CONSTRAINT "UserOnAssessment_assessmentId_fkey" FOREIGN KEY ("assessmentId") REFERENCES "Assessment"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "OwnerOnAssessment" ADD CONSTRAINT "OwnerOnAssessment_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "OwnerOnAssessment" ADD CONSTRAINT "OwnerOnAssessment_assessmentId_fkey" FOREIGN KEY ("assessmentId") REFERENCES "Assessment"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "_TagToTask" ADD CONSTRAINT "_TagToTask_A_fkey" FOREIGN KEY ("A") REFERENCES "Tag"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "_TagToTask" ADD CONSTRAINT "_TagToTask_B_fkey" FOREIGN KEY ("B") REFERENCES "Task"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+
+CREATE SCHEMA IF NOT EXISTS  extensions;
+CREATE extension IF NOT EXISTS "uuid-ossp" WITH SCHEMA extensions;
+CREATE SCHEMA IF NOT EXISTS reatlime;
+
+begin;
+  DROP publication IF EXISTS supabase_realtime;
+  CREATE publication supabase_realtime;
+commit;
+
+ALTER publication supabase_realtime ADD TABLE "Submission";
+ALTER TABLE "Submission" REPLICA IDENTITY full;
+
+
