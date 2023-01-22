b507618ed51af8d1fc22bd03ec10bcbb82f4ec17
MasterIceZ <64594596+MasterIceZ@users.noreply.github.com>
2023-01-22 14:01:07 +0700

    fix: overlapping text in task page

--- src/components/Task/Layout.tsx
+++ src/components/Task/Layout.tsx
@@ -2,6 +2,7 @@ import { Task } from '@prisma/client'
 
 import { PageLayout } from '@/components/Layout'
 import { SideBar } from '@/components/Task/SideBar'
+import { Title } from '@/components/Task/Title'
 
 export const TaskLayout = ({
   task,
@@ -15,10 +16,17 @@ export const TaskLayout = ({
   assessmentId?: string
 }) => (
   <PageLayout>
-    <div className="align-center flex h-full w-screen justify-center px-4 py-8">
-      <div className="flex h-full w-full max-w-6xl flex-col gap-2 md:flex-row md:gap-8">
-        <SideBar task={task} type={type} assessmentId={assessmentId} />
-        {children}
+    <div>
+      <div className="align-center flex h-full w-screen justify-center px-4 pt-4">
+        <div className="flex h-full w-full max-w-6xl flex-col gap-2 md:flex-row md:gap-8">
+          <Title task={task} type={type} assessmentId={assessmentId} />
+        </div>
+      </div>
+      <div className="align-center flex h-full w-screen justify-center px-4 py-4">
+        <div className="flex h-full w-full max-w-6xl flex-col gap-2 md:flex-row md:gap-8">
+          <SideBar task={task} type={type} assessmentId={assessmentId} />
+          {children}
+        </div>
       </div>
     </div>
   </PageLayout>
---
+++ src/components/Task/Title.tsx
@@ -0,0 +1,80 @@
+import { StarIcon as StarIconOutline } from '@heroicons/react/outline'
+import { StarIcon as StarIconSolid } from '@heroicons/react/solid'
+import { Task } from '@prisma/client'
+import clsx from 'clsx'
+import { useSession } from 'next-auth/react'
+import useSWR, { mutate } from 'swr'
+
+import fetcher from '@/lib/fetcher'
+
+export const Title = ({
+  task,
+  type,
+  assessmentId
+}: {
+  task: Task
+  type: string
+  assessmentId?: string
+}) => {
+  const { status } = useSession()
+
+  const { data: bookmark } = useSWR<boolean>(
+    task && status === 'authenticated'
+      ? `/api/bookmarks/task/${task.id}`
+      : null,
+    fetcher
+  )
+
+  return (
+    <div className="z-10 flex w-full flex-row-reverse justify-between space-x-2 px-2 md:flex-row md:justify-start md:px-0">
+      {!assessmentId && (
+        <button
+          className="mt-1 flex"
+          onClick={async () => {
+            mutate(
+              `/api/bookmarks/task/${task.id}`,
+              async (state: boolean) => {
+                await fetch(`/api/bookmarks`, {
+                  method: state ? 'POST' : 'DELETE',
+                  headers: { 'Content-Type': 'application/json' },
+                  body: JSON.stringify({ taskId: task.id })
+                })
+                return state
+              },
+              { optimisticData: !bookmark }
+            )
+          }}
+        >
+          {bookmark ? (
+            <StarIconSolid className="h-5 w-5 text-gray-400 dark:text-amber-400" />
+          ) : (
+            <StarIconOutline className="h-5 w-5 text-gray-300" />
+          )}
+        </button>
+      )}
+      <div
+        className={clsx(
+          'flex w-full flex-col',
+          task === undefined && 'animate-pulse'
+        )}
+      >
+        <h1
+          className={clsx(
+            'word-break-normal h-6 w-full text-lg font-medium dark:text-white',
+            task === undefined && 'rounded bg-gray-200 dark:bg-slate-700'
+          )}
+        >
+          {task?.title}
+        </h1>
+        <p
+          className={clsx(
+            'z-50 mt-1 h-5 w-full font-light dark:text-white',
+            task === undefined && 'rounded bg-gray-200 dark:bg-slate-700'
+          )}
+        >
+          {task?.id}
+        </p>
+      </div>
+    </div>
+  )
+}
