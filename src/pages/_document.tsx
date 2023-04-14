6493db37505bc03fea67b41129be4a4eda2195d4
iammarkps <markkung08@gmail.com>
2023-04-14 15:10:59 +0700

    feat: add Vercel analytics

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -7,7 +7,6 @@ import Document, {
 } from 'next/document'
 
 import { Head } from 'components/head'
-import { GA_TRACKING_ID } from 'lib/gtag'
 
 export default class MyDocument extends Document {
   static async getInitialProps(ctx: DocumentContext) {
@@ -20,22 +19,6 @@ export default class MyDocument extends Document {
       <Html lang="en">
         <NextHead>
           <Head />
-          <script
-            async
-            src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
-          />
-          <script
-            dangerouslySetInnerHTML={{
-              __html: `
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', '${GA_TRACKING_ID}', {
-              page_path: window.location.pathname,
-            });
-          `,
-            }}
-          />
         </NextHead>
         <body>
           <Main />
