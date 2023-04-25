e8a1e366fb5a24f418c4c364436a8ba51b4e535b
Pongsaphol Pongsawakul <ppongsawakul@wisc.edu>
2023-04-25 09:29:15 -0500

    change awsURL

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,6 @@
 export const config = {
   baseURL: 'https://asia-east2-proginth.cloudfunctions.net',
-  awsURL: 'https://beta-programming-in-th.s3-ap-southeast-1.amazonaws.com',
+  awsURL: 'hhttps://proginth.sgp1.cdn.digitaloceanspaces.com',
   languageData: [
     ['cpp14', 'C++', 'cpp'],
     ['c11', 'C', 'c'],
