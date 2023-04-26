3c549e71bc58877fb5caf84c8b137dacafba8076
Pongsaphol Pongsawakul <ppongsawakul@wisc.edu>
2023-04-26 15:19:45 -0500

    Revert "feat: add Vercel analytics"
    This reverts commit 6493db37505bc03fea67b41129be4a4eda2195d4.

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -7,6 +7,7 @@ import Document, {
 } from 'next/document'
 
 import { Head } from 'components/head'
+import { GA_TRACKING_ID } from 'lib/gtag'
 
 export default class MyDocument extends Document {
   static async getInitialProps(ctx: DocumentContext) {
@@ -19,6 +20,22 @@ export default class MyDocument extends Document {
       <Html lang="en">
         <NextHead>
           <Head />
+          <script
+            async
+            src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
+          />
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${GA_TRACKING_ID}', {
+              page_path: window.location.pathname,
+            });
+          `,
+            }}
+          />
         </NextHead>
         <body>
           <Main />
