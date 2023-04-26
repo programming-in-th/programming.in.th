bf82b0e2a0e2de257fe22825f1565daa67b75a34
Pongsaphol Pongsawakul <ppongsawakul@wisc.edu>
2023-04-26 15:16:46 -0500

    Revert "Revert "feat: add Vercel analytics""
    This reverts commit af3ab65a76d142f05cc5e121b8ae90c6ffd795e3.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,12 @@
 import { NextPage } from 'next'
 import { AppProps } from 'next/app'
 import Router from 'next/router'
-import React, { useEffect } from 'react'
+import React from 'react'
 import NProgress from 'nprogress'
 import { ThemeProvider, CSSReset } from '@chakra-ui/core'
 import UserProvider from 'components/UserContext'
-import * as gtag from 'lib/gtag'
+import { Analytics } from '@vercel/analytics/react'
+
 
 import { GlobalStyle } from 'design'
 import { customTheme } from 'design/theme'
@@ -28,22 +29,13 @@ Router.events.on('routeChangeComplete', done)
 Router.events.on('routeChangeError', done)
 
 const App: NextPage<AppProps> = ({ Component, pageProps }) => {
-  useEffect(() => {
-    const handleRouteChange = (url) => {
-      gtag.pageview(url)
-    }
-    Router.events.on('routeChangeComplete', handleRouteChange)
-    return () => {
-      Router.events.off('routeChangeComplete', handleRouteChange)
-    }
-  }, [])
-
   return (
     <ThemeProvider theme={customTheme}>
       <UserProvider>
         <CSSReset />
         <GlobalStyle />
         <Component {...pageProps} />
+        <Analytics />
       </UserProvider>
     </ThemeProvider>
   )
