e379edb46b078763826361329360d62062a58e78
Nutthapat Pongtanyavichai <59821765+Leomotors@users.noreply.github.com>
2023-06-02 21:24:10 +0700

    fix(solution): missing katex css

--- src/components/Task/Tabs/Solution.tsx
+++ src/components/Task/Tabs/Solution.tsx
@@ -1,10 +1,10 @@
 'use client'
 
-import Head from 'next/head'
-
 import { MDXRemote, MDXRemoteSerializeResult } from 'next-mdx-remote'
 
 import components from '@/components/common/MDXComponents'
+
+import 'katex/dist/katex.min.css'
 
 const articleClsx = 'prose mt-4 w-full max-w-none dark:text-gray-100'
 
@@ -32,26 +32,16 @@ const SolutionTab = ({
   }
 
   return (
-    <>
-      <Head>
-        <link
-          rel="stylesheet"
-          href="https://cdn.jsdelivr.net/npm/katex@0.16.0/dist/katex.min.css"
-          integrity="sha384-Xi8rHCmBmhbuyyhbI88391ZKP2dmfnOl4rT9ZfRI7mLTdk1wblIUnrIq35nqwEvC"
-          crossOrigin="anonymous"
-        />
-      </Head>
-      <article className={articleClsx}>
-        <MDXRemote
-          {...solution}
-          components={
-            {
-              ...components
-            } as any
-          }
-        />
-      </article>
-    </>
+    <article className={articleClsx}>
+      <MDXRemote
+        {...solution}
+        components={
+          {
+            ...components
+          } as any
+        }
+      />
+    </article>
   )
 }
 
