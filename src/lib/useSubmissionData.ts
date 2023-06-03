bd57988f4f3d7b613e748f6fe3b13e3fd17e1976
ImSoZRious <30285202+ImSoZRious@users.noreply.github.com>
2023-06-03 07:12:11 +0700

    feat/realtimedb (#807)

--- src/lib/useSubmissionData.ts
+++ src/lib/useSubmissionData.ts
@@ -1,5 +1,7 @@
 'use client'
 
+import { useEffect } from 'react'
+
 import useSWR from 'swr'
 
 import { IGeneralSubmission } from '@/types/submissions'
@@ -17,45 +19,44 @@ export const useSSESubmissionData = (id: number) => {
     fetcher
   )
 
-  // useEffect(() => {
-  //   const eventSource = new EventSource(
-  //     `${process.env.NEXT_PUBLIC_REALTIME_URL}?id=${id}`
-  //   )
+  useEffect(() => {
+    const eventSource = new EventSource(
+      `${process.env.NEXT_PUBLIC_REALTIME_URL}/${id}`
+    )
 
-  //   eventSource.onmessage = event => {
-  //     const realtimeData = JSON.parse(`${event.data}`)
+    eventSource.onmessage = event => {
+      const realtimeData = JSON.parse(`${event.data}`)
 
-  //     if (
-  //       realtimeData.status === JUDGE_COMPLETED ||
-  //       realtimeData.status === JUDGE_ERROR ||
-  //       realtimeData.status === JUDGE_COMPILATION_ERROR
-  //     ) {
-  //       mutate()
-  //       eventSource.close()
-  //     } else {
-  //       mutate(
-  //         async submission => {
-  //           return submission
-  //             ? Object.assign({}, submission, realtimeData)
-  //             : submission
-  //         },
-  //         {
-  //           revalidate: false
-  //         }
-  //       )
-  //     }
-  //   }
+      if (
+        realtimeData.status === JUDGE_COMPLETED ||
+        realtimeData.status === JUDGE_ERROR ||
+        realtimeData.status === JUDGE_COMPILATION_ERROR
+      ) {
+        mutate()
+        eventSource.close()
+      } else {
+        mutate(
+          async submission => {
+            return submission
+              ? Object.assign({}, submission, realtimeData)
+              : submission
+          },
+          {
+            revalidate: false
+          }
+        )
+      }
+    }
 
-  //   return () => {
-  //     eventSource.close()
-  //   }
-  // }, [id, mutate])
+    return () => {
+      eventSource.close()
+    }
+  }, [id, mutate])
 
   return {
     submission: data,
     isLoading: !error && !data,
-    isError: error,
-    mutate
+    isError: error
   }
 }
 
