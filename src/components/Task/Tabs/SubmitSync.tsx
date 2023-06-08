2c044237d7eb196d3d8cabb60eeb67d56cdcf8ac
Nutthapat Pongtanyavichai <59821765+Leomotors@users.noreply.github.com>
2023-06-08 23:25:42 +0700

    fix: page dir ts error

---
+++ src/components/Task/Tabs/SubmitSync.tsx
@@ -0,0 +1,36 @@
+'use client'
+
+import { Task } from '@prisma/client'
+import { useSession } from 'next-auth/react'
+
+import { SubmitElement } from '../SubmitElement'
+
+/**
+ * @deprecated For legacy page dir
+ */
+const SubmitTab = ({
+  task,
+  assessmentId
+}: {
+  task: Task
+  assessmentId?: string
+}) => {
+  const { status } = useSession()
+
+  if (status !== 'authenticated') {
+    return (
+      <div className="flex h-24 items-center justify-center">
+        <p className="text-center text-prog-gray-500 dark:text-gray-200">
+          You must be logged in to submit code
+        </p>
+      </div>
+    )
+  } else
+    return (
+      <div>
+        <SubmitElement task={task} assessmentId={assessmentId} />
+      </div>
+    )
+}
+
+export default SubmitTab
