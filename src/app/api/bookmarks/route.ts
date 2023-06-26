651c85ff350cf6789bc7b972a57979c1431e8b84
Mond45 <mondc45@gmail.com>
2023-06-26 16:55:38 +0700

    feat: add api query bookmarks for a user

---
+++ src/app/api/bookmarks/route.ts
@@ -0,0 +1,25 @@
+import { NextRequest, NextResponse } from 'next/server'
+
+import prisma from '@/lib/prisma'
+import { getServerUser } from '@/lib/session'
+
+export async function GET(_: NextRequest) {
+  const user = await getServerUser()
+
+  if (!user || !user.id) {
+    return NextResponse.json({ error: 'Unauthorized' }, { status: 401 })
+  }
+
+  const rawBookmark = await prisma.bookmark.findMany({
+    where: {
+      user: {
+        id: { equals: user?.id }
+      }
+    }
+  })
+
+  return NextResponse.json(
+    rawBookmark.map(bookmark => bookmark.taskId),
+    { status: 200 }
+  )
+}
