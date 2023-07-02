cce1b4776f13e76fcc5e929b2007776db3600831
Mond45 <mondc45@gmail.com>
2023-07-03 00:36:05 +0700

    feat: add skeleton for navbar user

--- src/components/RootLayout/Navbar.tsx
+++ src/components/RootLayout/Navbar.tsx
@@ -19,7 +19,7 @@ import {
 } from './useClientMirror'
 
 export const Navbar = () => {
-  const { data: session } = useSession()
+  const { data: session, status } = useSession()
   return (
     <NavbarBox>
       <div className="py-3">
@@ -57,57 +57,61 @@ export const Navbar = () => {
             </div>
             <div className="hidden items-center space-x-8 md:ml-10 md:flex">
               <DesktopLinks />
-              <div className="flex items-center space-x-4 pl-2">
-                {session?.user ? (
-                  <Popover className="relative hidden md:block">
-                    <PopoverButton className="flex h-10 w-10 items-center justify-center rounded-full bg-transparent ring-slate-300 transition-colors hover:bg-slate-300 hover:bg-opacity-50 active:ring-1">
-                      <Image
-                        src={
-                          session?.user.image ??
-                          '/assets/img/profile/default.svg'
-                        }
-                        alt={session?.user.name ?? 'Default profile image'}
-                        width={32}
-                        height={32}
-                        className="h-8 w-8 rounded-full"
-                        style={{
-                          maxWidth: '100%',
-                          height: 'auto'
-                        }}
-                      />
-                    </PopoverButton>
-
-                    <Transition
-                      as={Fragment}
-                      enter="duration-150 ease-out"
-                      enterFrom="opacity-0 scale-95"
-                      enterTo="opacity-100 scale-100"
-                      leave="duration-100 ease-in"
-                      leaveFrom="opacity-100 scale-100"
-                      leaveTo="opacity-0 scale-95"
-                    >
-                      <PopoverPanel className="absolute right-2 z-50 mt-2 rounded-lg bg-white shadow-md dark:bg-slate-800">
-                        <div className="flex flex-col border-b border-gray-100 px-8 py-4 text-prog-gray-500 dark:border-slate-900 dark:text-gray-100">
-                          <p className="font-medium">
-                            {session?.user.username}
-                          </p>
-                          <p className="font-light">{session?.user.email}</p>
-                        </div>
-
-                        <div className="px-8 py-4">
-                          <SignoutButton className="text-prog-gray-500 hover:text-gray-700 dark:text-gray-100 dark:hover:text-gray-400" />
-                        </div>
-                      </PopoverPanel>
-                    </Transition>
-                  </Popover>
-                ) : (
-                  <Link
-                    href="/login"
-                    className="rounded-md bg-prog-primary-500 px-6 py-2 text-base font-medium text-white shadow-md transition-colors hover:bg-prog-primary-600"
-                  >
-                    Login
-                  </Link>
+              <div className="flex h-10 w-24 items-center space-x-4 pl-2">
+                {status === 'loading' && (
+                  <div className="h-full w-full animate-pulse rounded-md bg-gray-400" />
                 )}
+                {status !== 'loading' &&
+                  (session?.user ? (
+                    <Popover className="relative hidden md:block">
+                      <PopoverButton className="flex h-10 w-10 items-center justify-center rounded-full bg-transparent ring-slate-300 transition-colors hover:bg-slate-300 hover:bg-opacity-50 active:ring-1">
+                        <Image
+                          src={
+                            session?.user.image ??
+                            '/assets/img/profile/default.svg'
+                          }
+                          alt={session?.user.name ?? 'Default profile image'}
+                          width={32}
+                          height={32}
+                          className="h-8 w-8 rounded-full"
+                          style={{
+                            maxWidth: '100%',
+                            height: 'auto'
+                          }}
+                        />
+                      </PopoverButton>
+
+                      <Transition
+                        as={Fragment}
+                        enter="duration-150 ease-out"
+                        enterFrom="opacity-0 scale-95"
+                        enterTo="opacity-100 scale-100"
+                        leave="duration-100 ease-in"
+                        leaveFrom="opacity-100 scale-100"
+                        leaveTo="opacity-0 scale-95"
+                      >
+                        <PopoverPanel className="absolute right-2 z-50 mt-2 rounded-lg bg-white shadow-md dark:bg-slate-800">
+                          <div className="flex flex-col border-b border-gray-100 px-8 py-4 text-prog-gray-500 dark:border-slate-900 dark:text-gray-100">
+                            <p className="font-medium">
+                              {session?.user.username}
+                            </p>
+                            <p className="font-light">{session?.user.email}</p>
+                          </div>
+
+                          <div className="px-8 py-4">
+                            <SignoutButton className="text-prog-gray-500 hover:text-gray-700 dark:text-gray-100 dark:hover:text-gray-400" />
+                          </div>
+                        </PopoverPanel>
+                      </Transition>
+                    </Popover>
+                  ) : (
+                    <Link
+                      href="/login"
+                      className="rounded-md bg-prog-primary-500 px-6 py-2 text-base font-medium text-white shadow-md transition-colors hover:bg-prog-primary-600"
+                    >
+                      Login
+                    </Link>
+                  ))}
               </div>
             </div>
           </div>
