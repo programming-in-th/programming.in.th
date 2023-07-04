ff3dccc2b7dafb8c24f63c138a69ddc5464e3208
Pongsaphol Pongsawakul <ppongsawakul@wisc.edu>
2023-07-04 22:22:54 +0700

    feat: add tost provider

--- src/components/RootLayout/Providers.tsx
+++ src/components/RootLayout/Providers.tsx
@@ -4,6 +4,7 @@ import { ReactNode } from 'react'
 
 import { SessionProvider } from 'next-auth/react'
 import { ThemeProvider } from 'next-themes'
+import { Toaster } from 'react-hot-toast'
 
 /**
  * Providers for client components
@@ -11,6 +12,9 @@ import { ThemeProvider } from 'next-themes'
 export function Providers({ children }: { children: ReactNode }) {
   return (
     <SessionProvider>
+      <Toaster
+        toastOptions={{ className: 'dark:bg-slate-600 dark:text-gray-100' }}
+      />
       <ThemeProvider>{children}</ThemeProvider>
     </SessionProvider>
   )
