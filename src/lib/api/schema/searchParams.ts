8256236993425e2d17cd0c607b1a10750fc9dfac
Pongsaphol Pongsawakul <ppongsawakul@wisc.edu>
2023-07-05 00:55:07 +0700

    fix: parsed array

--- src/lib/api/schema/searchParams.ts
+++ src/lib/api/schema/searchParams.ts
@@ -1,6 +1,14 @@
 export function paramsToObject(search: URLSearchParams) {
-  const params: Record<string, string> = {}
+  const params: Record<string, string | string[]> = {}
   for (const [key, value] of search.entries()) {
+    if (params[key]) {
+      if (Array.isArray(params[key])) {
+        ;(params[key] as string[]).push(value)
+      } else {
+        params[key] = [params[key] as string, value]
+      }
+      continue
+    }
     params[key] = value
   }
   return params
