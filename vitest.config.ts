21c198b0bc2e7941f3bf49e233f0ed7cde0ce513
Nutthapat Pongtanyavichai <59821765+Leomotors@users.noreply.github.com>
2023-07-05 19:06:33 +0700

    fix: vitest include

--- vitest.config.ts
+++ vitest.config.ts
@@ -10,6 +10,7 @@ export default defineConfig({
     coverage: {
       provider: 'v8',
       reporter: ['text', 'html', 'json-summary', 'json']
-    }
+    },
+    include: ['src/**/*.spec.{ts,tsx}']
   }
 })
