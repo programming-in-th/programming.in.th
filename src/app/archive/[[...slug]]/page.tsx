dbacfde4256f380e5dcbc268734f382fe763fe30
Mond45 <mondc45@gmail.com>
2023-07-13 03:02:28 +0700

    feat: archive tasks list

--- src/app/archive/[[...slug]]/page.tsx
+++ src/app/archive/[[...slug]]/page.tsx
@@ -1,8 +1,12 @@
 import { notFound } from 'next/navigation'
 
+import { Prisma } from '@prisma/client'
+
 import Breadcrumb from '@/components/Archive/Breadcrumb'
 import CategoryList from '@/components/Archive/CategoryList'
 import TaskList from '@/components/Archive/TaskList'
+import prisma from '@/lib/prisma'
+import { IGeneralTask, ISolved } from '@/types/tasks'
 import getCategoryTree, { generatePath } from '@/utils/getCategoryTree'
 
 export default async function Archive({
@@ -19,10 +23,33 @@ export default async function Archive({
         {category?.childCategories && (
           <CategoryList categories={category.childCategories} />
         )}
-        {category?.childTasks && <TaskList tasks={category.childTasks} />}
+        {category?.childTasks && (
+          <div className="max-w-7xl grow">
+            <TaskList
+              tasks={category.childTasks}
+              {...await getTasksInfo(category.childTasks)}
+            />
+          </div>
+        )}
       </div>
     </div>
   )
+}
+
+async function getTasksInfo(tasks: IGeneralTask[]) {
+  const getSolved = async () => {
+    const rawSolved = await prisma.$queryRaw(
+      Prisma.sql`SELECT COUNT(DISTINCT submission.user_id), submission.task_id FROM submission INNER JOIN task ON submission.task_id = task.id WHERE submission.score = task.full_score GROUP BY submission.task_id`
+    )
+
+    return JSON.parse(
+      JSON.stringify(rawSolved, (_, v) =>
+        typeof v === 'bigint' ? `${v}n` : v
+      ).replace(/"(-?\d+)n"/g, (_, a) => a)
+    ) as ISolved[]
+  }
+  const solved = await getSolved()
+  return { solved, tags: Array.from(new Set(tasks.flatMap(task => task.tags))) }
 }
 
 async function generatePaths(slug?: string[]) {
