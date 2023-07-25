81d55b39a6aa0eba3548c8ab9dcf307d8c795ccd
Pongsaphol Pongsawakul <ppongsawakul@wisc.edu>
2023-07-25 11:30:35 +0700

    fix: submission on assessment

--- src/components/Task/Layout.tsx
+++ src/components/Task/Layout.tsx
@@ -21,7 +21,7 @@ export function TaskLayout({
     <div className="align-center flex h-full w-screen justify-center px-4 py-4">
       <div className="flex h-full w-full max-w-6xl flex-col gap-2 md:flex-row md:gap-8">
         <div className="flex flex-col gap-4 md:w-[18rem]">
-          <Title task={task} />
+          <Title task={task} assessmentId={assessmentId} />
           <SideBar task={task} type={type} assessmentId={assessmentId} />
         </div>
         {children}
