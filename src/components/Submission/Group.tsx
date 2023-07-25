8a1a3763b1b8d6ebc6e61b98726a6dae33bb27a8
Borworntat Dendumrongkul <64594596+MasterIceZ@users.noreply.github.com>
2023-07-25 10:53:58 +0700

    fix: render grouping testcases (#883)

--- src/components/Submission/Group.tsx
+++ src/components/Submission/Group.tsx
@@ -6,15 +6,15 @@ import GroupTable from './GroupTable'
 import SeparateTable from './SeparateTable'
 
 const SubmissionGroup = ({ groups }: { groups: IGroup[] }) => {
-  const isGroup = useMemo<boolean>(() => {
-    return groups.every(current => current.run_result.length !== 1)
+  const isSeperate = useMemo<boolean>(() => {
+    return groups.every(current => current.run_result.length === 1)
   }, [groups])
   return (
     <div className="flex w-full flex-col rounded-md bg-gray-50 p-5 dark:bg-slate-700 xl:px-10 xl:py-10">
-      {isGroup ? (
-        <GroupTable groups={groups} />
-      ) : (
+      {isSeperate ? (
         <SeparateTable groups={groups} />
+      ) : (
+        <GroupTable groups={groups} />
       )}
     </div>
   )
