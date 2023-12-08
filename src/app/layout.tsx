25d3497d7d21e3a16886c6e42df5ddb0f64830d0
Possawat Suksai <markkung08@gmail.com>
2023-12-08 16:06:33 +0700

    chore: migrate Vercel's speed-insight (#938)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { ReactNode } from 'react'
 import { Metadata, Viewport } from 'next'
 
 import { Analytics } from '@vercel/analytics/react'
+import { SpeedInsights } from '@vercel/speed-insights/next'
 import { Inter, Noto_Sans_Thai } from 'next/font/google'
 
 import '@/styles/index.css'
@@ -42,6 +43,7 @@ export default async function RootLayout({
             <Footer />
           </LayoutWithTheme>
         </Providers>
+        <SpeedInsights />
       </body>
       <Analytics />
     </html>
